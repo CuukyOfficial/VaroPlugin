079b8e45e19d88d4ab622015d040b0f5b2eb2b91
CuukyOfficial <just.cookie.jc@gmail.com>
2020-01-27 23:03:34 +0100

    Sorted AlertType code

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/alert/AlertType.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/alert/AlertType.java
@@ -7,20 +7,28 @@ public enum AlertType implements VaroSerializeable {
 
 	@VaroSerializeField(enumValue = "BLOODLUST")
 	BLOODLUST("BLOODLUST"),
+	
 	@VaroSerializeField(enumValue = "COMBATLOG")
 	COMBATLOG("COMBATLOG"),
+	
 	@VaroSerializeField(enumValue = "DISCONNECT")
 	DISCONNECT("DISCONNECT"),
+	
 	@VaroSerializeField(enumValue = "INVENTORY_CLEAR")
 	INVENTORY_CLEAR("INVENTORY_CLEAR"),
+	
 	@VaroSerializeField(enumValue = "NAME_SWITCH")
 	NAME_SWITCH("NAME_SWITCH"),
+	
 	@VaroSerializeField(enumValue = "NO_JOIN")
 	NO_JOIN("NO_JOIN"),
+	
 	@VaroSerializeField(enumValue = "NO_YOUTUBE_UPLOAD")
 	NO_YOUTUBE_UPLOAD("NO_YOUTUBE_UPLOAD"),
+	
 	@VaroSerializeField(enumValue = "STRIKE")
 	STRIKE("STRIKE"),
+	
 	@VaroSerializeField(enumValue = "UPDATE_AVAILABLE")
 	UPDATE_AVAILABLE("UPDATE_AVAILABLE");
 
