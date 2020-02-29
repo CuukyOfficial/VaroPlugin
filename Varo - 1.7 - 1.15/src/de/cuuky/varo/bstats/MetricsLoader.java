997170ace06bce1f40d5a914c7a8d8ae3d0bfb23
CuukyOfficial <just.cookie.jc@gmail.com>
2020-02-29 19:06:04 +0100

    Improved MetricsLoader

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/bstats/MetricsLoader.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/bstats/MetricsLoader.java
@@ -6,6 +6,7 @@ import java.util.concurrent.Callable;
 
 import org.bukkit.plugin.java.JavaPlugin;
 
+import de.cuuky.varo.Main;
 import de.cuuky.varo.configuration.config.ConfigEntry;
 import de.cuuky.varo.threads.LagCounter;
 
@@ -20,41 +21,46 @@ public class MetricsLoader {
 	}
 	
 	private void loadMetrics(JavaPlugin instance) {
-		metrics = new Metrics(instance, BSTATS_ID);
-		metrics.addCustomChart(new Metrics.SimpleBarChart("functionsUsed", new Callable<Map<String, Integer>>() {
+		try {
+			metrics = new Metrics(instance, BSTATS_ID);
+			metrics.addCustomChart(new Metrics.SimpleBarChart("functionsUsed", new Callable<Map<String, Integer>>() {
 
-			@Override
-			public Map<String, Integer> call() throws Exception {
-				Map<String, Integer> map = new HashMap<>();
-				for(ConfigEntry entry : ConfigEntry.values()) {
-					if(entry.getValue() instanceof Boolean) {
-						map.put(entry.getName(), entry.getValueAsBoolean() ? 1 : 0);
+				@Override
+				public Map<String, Integer> call() throws Exception {
+					Map<String, Integer> map = new HashMap<>();
+					for(ConfigEntry entry : ConfigEntry.values()) {
+						if(entry.getValue() instanceof Boolean) {
+							map.put(entry.getName(), entry.getValueAsBoolean() ? 1 : 0);
+						}
 					}
+
+					return map;
 				}
+			}));
 
-				return map;
-			}
-		}));
-
-		metrics.addCustomChart(new Metrics.MultiLineChart("functionsValues", new Callable<Map<String, Integer>>() {
-			@Override
-			public Map<String, Integer> call() throws Exception {
-				Map<String, Integer> valueMap = new HashMap<>();
-				for(ConfigEntry entry : ConfigEntry.values()) {
-					if(entry.getValue() instanceof Integer) {
-						valueMap.put(entry.getName(), entry.getValueAsInt());
+			metrics.addCustomChart(new Metrics.MultiLineChart("functionsValues", new Callable<Map<String, Integer>>() {
+				@Override
+				public Map<String, Integer> call() throws Exception {
+					Map<String, Integer> valueMap = new HashMap<>();
+					for(ConfigEntry entry : ConfigEntry.values()) {
+						if(entry.getValue() instanceof Integer) {
+							valueMap.put(entry.getName(), entry.getValueAsInt());
+						}
 					}
+
+					return valueMap;
 				}
+			}));
 
-				return valueMap;
-			}
-		}));
-
-		metrics.addCustomChart(new Metrics.SingleLineChart("serverTps", new Callable<Integer>() {
-			@Override
-			public Integer call() throws Exception {
-				return (int) Math.round(LagCounter.getTPS());
-			}
-		}));
+			metrics.addCustomChart(new Metrics.SingleLineChart("serverTps", new Callable<Integer>() {
+				@Override
+				public Integer call() throws Exception {
+					return (int) Math.round(LagCounter.getTPS());
+				}
+			}));
+		} catch(Exception e) {
+			e.printStackTrace();
+			System.err.println(Main.getPrefix() + "Failed to send data to bStats!");
+		}
 	}
 }
