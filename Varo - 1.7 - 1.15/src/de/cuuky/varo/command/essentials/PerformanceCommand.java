a06f343a3d4833353db26739b3c1cd24bc91be90
CuukyOfficial <just.cookie.jc@gmail.com>
2020-03-08 10:35:40 +0100

    Improved Performance command

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/command/essentials/PerformanceCommand.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/command/essentials/PerformanceCommand.java
@@ -5,8 +5,9 @@ import org.bukkit.World;
 import org.bukkit.command.Command;
 import org.bukkit.command.CommandExecutor;
 import org.bukkit.command.CommandSender;
+import org.bukkit.entity.ArmorStand;
 import org.bukkit.entity.Entity;
-import org.bukkit.entity.Player;
+import org.bukkit.entity.LivingEntity;
 
 import de.cuuky.varo.Main;
 import de.cuuky.varo.configuration.config.ConfigEntry;
@@ -23,19 +24,23 @@ public class PerformanceCommand implements CommandExecutor {
 		}
 
 		if(args.length == 0) {
-			sender.sendMessage(Main.getPrefix() + Main.getColorCode() + "/performance clear §8- §7Führt einen RAM-Cleaner aus");
-			sender.sendMessage(Main.getPrefix() + Main.getColorCode() + "/performance help §8- §7Zeigt Methoden zur Performanceverbesserung");
-			sender.sendMessage(Main.getPrefix() + Main.getColorCode() + "/performance entityclear §8- §7Entfernt Items auf dem Boden, Tiere etc. (alles außer Spielern)");
-			sender.sendMessage(Main.getPrefix() + Main.getColorCode() + "TIPP: §7/usage zeigt die Ausnutzung deines Servers");
+			sender.sendMessage(Main.getPrefix() + "§c§lPerformance Command§7§l:");
+			sender.sendMessage(Main.getPrefix() + "§c/performance clear §8- §7Führt einen RAM-Cleaner aus");
+			sender.sendMessage(Main.getPrefix() + "§c/performance help §8- §7Zeigt Methoden zur Performanceverbesserung");
+			sender.sendMessage(Main.getPrefix() + "§c/performance entityclear §8- §7Entfernt Items auf dem Boden etc. (ausgenommen Spieler, ArmorStands, Tiere)");
+			sender.sendMessage(Main.getPrefix() + "§cTIPP: §7/usage zeigt die Ausnutzung deines Servers");
 			return false;
 		}
 
 		if(args[0].equalsIgnoreCase("improve") || args[0].equalsIgnoreCase("clear")) {
+			Runtime r = Runtime.getRuntime();
+			double ramUsage = (r.totalMemory() - r.freeMemory()) / 1048576;
 			System.gc();
-			sender.sendMessage(Main.getPrefix() + "RAM weitesgehend entleert!");
+			double ramCleared = ramUsage - (r.totalMemory() - r.freeMemory()) / 1048576;
+			sender.sendMessage(Main.getPrefix() + "RAM wurde um §c" + ramCleared + "MB §7geleert!");
 		} else if(args[0].equalsIgnoreCase("help")) {
 			sender.sendMessage(Main.getPrefix() + "Derzeitige TPS: §c" + Math.round(LagCounter.getTPS()) + "§7 - Normalwert §c18-20 §7TPS");
-			
+
 			sender.sendMessage(Main.getPrefix() + "Folgende Einstellungen könnten die Performance vermindern - das Ausschalten erhoeht eventuell die Performance:");
 			sender.sendMessage(Main.getPrefix());
 			for(ConfigEntry ce : ConfigEntry.values())
@@ -44,17 +49,19 @@ public class PerformanceCommand implements CommandExecutor {
 
 			int entities = 0;
 			for(World world : Bukkit.getWorlds())
-				entities += world.getEntities().size();
+				for(Entity entity : world.getEntities())
+					if(!(entity instanceof ArmorStand) && !(entity instanceof LivingEntity))
+						entities++;
 
-			sender.sendMessage(Main.getPrefix() + "Es sind §c" + entities + " §7Entities (inklusive Spieler, ArmorStands, Tiere etc.) geladen - alle nicht-Spieler zu entfernen könnte die Performance erhöhen");
+			sender.sendMessage(Main.getPrefix() + "Es sind §c" + entities + " §7Entities (ausgenommen Spieler, ArmorStands, Tiere) geladen - alle nicht-Spieler zu entfernen könnte die Performance erhöhen");
 			sender.sendMessage(Main.getPrefix() + "Es sind §c" + Bukkit.getPluginManager().getPlugins().length + " §7Plugins aktiviert - bitte alle nicht nötigen entfernen");
 		} else if(args[0].equalsIgnoreCase("entityclear")) {
 			for(World world : Bukkit.getWorlds())
 				for(Entity entity : world.getEntities())
-					if(!(entity instanceof Player))
+					if(!(entity instanceof ArmorStand) && !(entity instanceof LivingEntity))
 						entity.remove();
 
-			sender.sendMessage(Main.getPrefix() + "Alle Nicht-Spieler entfernt!");
+			sender.sendMessage(Main.getPrefix() + "Alle Nicht- Spieler,Tiere oder ArmorStands entfernt!");
 		}
 
 		return false;
