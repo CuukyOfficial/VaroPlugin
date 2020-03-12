14b83232c302c76a1fe4383f92225a064dbfe8bd
CuukyOfficial <just.cookie.jc@gmail.com>
2020-03-12 21:27:00 +0100

    Added %size% placeholder to minuteTimeBorderDecrease

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/game/world/border/decrease/BorderDecreaseMinuteTimer.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/game/world/border/decrease/BorderDecreaseMinuteTimer.java
@@ -34,7 +34,7 @@ public class BorderDecreaseMinuteTimer {
 					Main.getVaroGame().getVaroWorld().getVaroBorder().decreaseBorder(DecreaseReason.TIME_MINUTES);
 					secondsPassed = timer;
 				} else if(secondsPassed % ConfigSetting.BORDER_TIME_MINUTE_BC_INTERVAL.getValueAsInt() == 0 && secondsPassed != timer)
-					Bukkit.broadcastMessage(ConfigMessages.BORDER_MINUTE_TIME_UPDATE.getValue().replace("%minutes%", getCountdownMin(secondsPassed)).replace("%seconds%", getCountdownSec(secondsPassed)));
+					Bukkit.broadcastMessage(ConfigMessages.BORDER_MINUTE_TIME_UPDATE.getValue().replace("%minutes%", getCountdownMin(secondsPassed)).replace("%seconds%", getCountdownSec(secondsPassed)).replace("%size%", String.valueOf(ConfigSetting.BORDER_TIME_MINUTE_DECREASE_SIZE.getValueAsInt())));
 
 				secondsPassed--;
 			}
