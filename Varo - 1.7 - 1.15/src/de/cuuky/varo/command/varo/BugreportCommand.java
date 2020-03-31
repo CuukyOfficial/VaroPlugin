119bfc5be7703f2c5852f6f9bf88b41c91e1b1a1
UeberallGebannt <paulmertens63@gmail.com>
2020-03-31 09:53:46 +0200

    Resolved Merge conflicts

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/command/varo/BugreportCommand.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/command/varo/BugreportCommand.java
@@ -5,34 +5,49 @@ import org.bukkit.command.CommandSender;
 
 import de.cuuky.varo.Main;
 import de.cuuky.varo.command.VaroCommand;
-import de.cuuky.varo.configuration.configurations.messages.ConfigMessages;
 import de.cuuky.varo.entity.player.VaroPlayer;
+import de.cuuky.varo.recovery.FileUploader;
 import de.cuuky.varo.recovery.recoveries.VaroBugreport;
 import de.cuuky.varo.spigot.updater.VaroUpdateResultSet.UpdateResult;
 
 public class BugreportCommand extends VaroCommand {
 
 	public BugreportCommand() {
-		super("bugreport", "Hift bei der Fehlersuche und beim Reporten von Bugs", "varo.bug", "bug", "bughelp", "error");
+		super("bugreport", "Hift bei der Fehlersuche und beim Reporten von Bugs", "varo.bug", "bug", "bughelp", "error", "support");
 	}
 
 	@Override
 	public void onCommand(CommandSender sender, VaroPlayer vp, Command cmd, String label, String[] args) {
 		if(Main.getVaroUpdater().getLastResult().getUpdateResult() == UpdateResult.UPDATE_AVAILABLE) {
-			sender.sendMessage(Main.getPrefix() + ConfigMessages.VARO_COMMANDS_BUGREPORT_OUTDATED_VERSION.getValue());
-			sender.sendMessage(Main.getPrefix() + ConfigMessages.VARO_COMMANDS_BUGREPORT_CURRENT_VERSION.getValue().replace("%version%", Main.getInstance().getDescription().getVersion().toString()));
-			sender.sendMessage(Main.getPrefix() + ConfigMessages.VARO_COMMANDS_BUGREPORT_NEWEST_VERSION.getValue().replace("%version%", Main.getVaroUpdater().getLastResult().getVersionName()));
-			sender.sendMessage(Main.getPrefix() + ConfigMessages.VARO_COMMANDS_BUGREPORT_UPDATE.getValue());
+			sender.sendMessage(Main.getPrefix() + "Du kannst keine Bugreports von einer alten Plugin-Version machen!");
+			sender.sendMessage(Main.getPrefix() + "Derzeitige Version: §c" + Main.getInstance().getDescription().getVersion());
+			sender.sendMessage(Main.getPrefix() + "Neueste Version: §a" + Main.getVaroUpdater().getLastResult().getVersionName());
+			sender.sendMessage(Main.getPrefix() + "§a/varo update");
 			return;
 		}
-		
+
+		sender.sendMessage(Main.getPrefix() + "Daten werden gesammelt...");
 		VaroBugreport bugreport = new VaroBugreport();
 		if(bugreport.hasFailed()) {
-			sender.sendMessage(Main.getPrefix() + ConfigMessages.VARO_COMMANDS_ERROR.getValue());
+			sender.sendMessage(Main.getPrefix() + "Ein Fehler ist aufgetreten!");
 			return;
 		}
+
+		sender.sendMessage(Main.getPrefix() + "Bugreport wurde unter §c" + bugreport.getZipFile().getPath() + " §7gespeichert!");
+		FileUploader uploader = new FileUploader(bugreport.getZipFile());
 		
-		sender.sendMessage(Main.getPrefix() + ConfigMessages.VARO_COMMANDS_BUGREPORT_CREATED.getValue().replace("%filename%", bugreport.getZipFile().getName()));
-		sender.sendMessage(Main.getPrefix() + ConfigMessages.VARO_COMMANDS_BUGREPORT_SEND_TO_DISCORD.getValue());
+		sender.sendMessage(Main.getPrefix() + "Lade Bugreport hoch...");
+		String url = uploader.uploadFile();
+
+		if(url == null) {
+			sender.sendMessage(Main.getPrefix() + "Der Bugreport konnte nicht hochgeladen werden!");
+			sender.sendMessage(Main.getPrefix() + "Bitte sende die Datei manuell auf den Discord in den Support!");
+		} else {
+			String message = Main.getPrefix() + "Der Bugreport wurde auf §c" + url + " §7hochgeladen!";
+			if(vp != null)
+				vp.getNetworkManager().sendLinkedMessage(message + " §7(§aKlick mich§7)", url);
+			else
+				sender.sendMessage(message);
+		}
 	}
 }
