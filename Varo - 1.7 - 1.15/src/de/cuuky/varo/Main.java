4cb8c498be5d6db1d1649729a04e5b881b8bcd58
Kornelius Rohrschneider <Korne127@gmx.net>
2020-04-07 01:24:56 +0200

    Fixed wrong licence information

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/Main.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/Main.java
@@ -26,7 +26,7 @@ import de.cuuky.varo.version.VersionUtils;
 public class Main extends JavaPlugin {
 
 	/*
-	 * Plugin by Cuuky @ 2019-2020 - All rights reserved! Contributors: Korne127
+	 * Plugin by Cuuky @ 2019-2020 - licensed under the GNU Affero General Public License v3.0 Contributors: Korne127
 	 */
 
 	private static final String CONSOLE_PREFIX = "[Varo] ";
