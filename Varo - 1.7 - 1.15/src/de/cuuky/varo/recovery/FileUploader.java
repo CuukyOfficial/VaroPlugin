7539d0a6b7d6379a9a5c3a560871c40db2e8cd4c
CuukyOfficial <just.cookie.jc@gmail.com>
2020-04-17 13:23:51 +0200

    Added FileUploader

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/recovery/FileUploader.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/recovery/FileUploader.java
@@ -1,17 +1,16 @@
 package de.cuuky.varo.recovery;
 
 import java.io.File;
-import java.io.IOException;
-import java.util.Scanner;
 
 import org.json.simple.JSONObject;
 import org.json.simple.JSONValue;
-import org.json.simple.parser.ParseException;
+
+import de.cuuky.varo.recovery.utils.MultipartUtility;
 
 public class FileUploader {
-	
+
 	private File file;
-	
+
 	public FileUploader(File file) {
 		this.file = file;
 	}
@@ -19,20 +18,23 @@ public class FileUploader {
 	private JSONObject getJSONObject(JSONObject object, String path) {
 		return (JSONObject) object.get(path);
 	}
-	
+
 	public String uploadFile() {
 		try {
-			Process process = Runtime.getRuntime().exec("curl -F \"file=@" + file.getPath() +"\" https://api.anonfile.com/upload?token=894b0ea821338221");
-			Scanner scanner = new Scanner(process.getInputStream());
-			while(scanner.hasNextLine()) {
-				JSONObject object = (JSONObject) JSONValue.parseWithException(scanner.nextLine());
-				scanner.close();
-				return (String) getJSONObject(getJSONObject(getJSONObject(object, "data"), "file"), "url").get("short");
-			}
-		} catch(IOException | ParseException e) {
+			MultipartUtility multipart = new MultipartUtility("https://api.anonfile.com/upload?token=894b0ea821338221", "UTF-8");
+			
+			multipart.addHeaderField("User-Agent", "CodeJava");
+            multipart.addHeaderField("Test-Header", "Header-Value");
+			
+			multipart.addFilePart("file", file);
+			String response = multipart.finish().get(0);
+			
+			JSONObject object = (JSONObject) JSONValue.parseWithException(response);
+			return (String) getJSONObject(getJSONObject(getJSONObject(object, "data"), "file"), "url").get("short");
+		} catch(Exception e) {
 			e.printStackTrace();
 		}
-		
+
 		return null;
 	}
 }
