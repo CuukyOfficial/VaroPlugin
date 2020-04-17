cfc548423b6755f9336acc3bfe8286ac624cff46
CuukyOfficial <just.cookie.jc@gmail.com>
2020-04-17 11:55:59 +0200

    Improved getting prefixes

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/utils/PermissionUtils.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/utils/PermissionUtils.java
@@ -1,14 +1,34 @@
 package de.cuuky.varo.utils;
 
+import java.util.ArrayList;
+import java.util.HashMap;
+
 import de.cuuky.varo.entity.player.VaroPlayer;
 
 public final class PermissionUtils {
 
-	private PermissionUtils() {}
+	private static HashMap<String, Class<?>> clazzes;
+
+	static {
+		clazzes = new HashMap<>();
+		ArrayList<Class<?>> toAdd = new ArrayList<>();
+
+		try {
+			toAdd.add(Class.forName("ru.tehkode.permissions.bukkit.PermissionsEx"));
+		} catch(Exception e) {}
+
+		try {
+			toAdd.add(Class.forName("me.lucko.luckperms.LuckPerms"));
+			toAdd.add(Class.forName("me.lucko.luckperms.api.Contexts"));
+		} catch(Exception e) {}
+
+		for(Class<?> clazz : toAdd)
+			clazzes.put(clazz.getName(), clazz);
+	}
 
 	public static String getLuckPermsPrefix(VaroPlayer player) {
 		try {
-			Object api = Class.forName("me.lucko.luckperms.LuckPerms").getDeclaredMethod("getApi").invoke(null);
+			Object api = clazzes.get("me.lucko.luckperms.LuckPerms").getDeclaredMethod("getApi").invoke(null);
 
 			Object user = api.getClass().getMethod("getUser", String.class).invoke(api, player.getUuid());
 			String groupname = (String) user.getClass().getMethod("getPrimaryGroup").invoke(api);
@@ -16,7 +36,7 @@ public final class PermissionUtils {
 			Object group = api.getClass().getMethod("getGroup", String.class).invoke(api, groupname);
 			Object cachedData = group.getClass().getMethod("getCachedData").invoke(group);
 
-			Object contexts = Class.forName("me.lucko.luckperms.api.Contexts").getDeclaredMethod("allowAll").invoke(null);
+			Object contexts = clazzes.get("me.lucko.luckperms.api.Contexts").getDeclaredMethod("allowAll").invoke(null);
 
 			Object metadata = cachedData.getClass().getMethod("getMetaData", contexts.getClass()).invoke(cachedData, contexts);
 			return (String) metadata.getClass().getMethod("getPrefix").invoke(metadata);
@@ -27,13 +47,19 @@ public final class PermissionUtils {
 
 	@SuppressWarnings("unchecked")
 	public static String getPermissionsExPrefix(VaroPlayer player) {
+		String prefix = "";
 		try {
-			Object permissionUser = Class.forName("ru.tehkode.permissions.bukkit.PermissionsEx").getDeclaredMethod("getUser", String.class).invoke(null, player.getName());
-			Object group = ((Object[]) permissionUser.getClass().getDeclaredMethod("getGroups").invoke(permissionUser))[0];
+			Object permissionUser = clazzes.get("ru.tehkode.permissions.bukkit.PermissionsEx").getDeclaredMethod("getUser", String.class).invoke(null, player.getName());
+			Object[] groups = ((Object[]) permissionUser.getClass().getDeclaredMethod("getGroups").invoke(permissionUser));
 
-			return (String) group.getClass().getMethod("getPrefix").invoke(group);
-		} catch(Throwable e) {}
+			if(groups.length > 1)
+				prefix = (String) groups[0].getClass().getMethod("getPrefix").invoke(groups[0]);
+			else 
+				prefix = (String) permissionUser.getClass().getMethod("getPrefix").invoke(permissionUser);
+		} catch(Throwable e) {
+			e.printStackTrace();
+		}
 
-		return "";
+		return prefix.replace("&", "§");
 	}
 }
