2818a52bbb75a90f824892be66954e6ec48d6a61
CuukyOfficial <just.cookie.jc@gmail.com>
2020-04-19 23:58:05 +0200

    Added message for chests

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/listener/saveable/SignChangeListener.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/listener/saveable/SignChangeListener.java
@@ -15,7 +15,6 @@ import org.bukkit.event.block.SignChangeEvent;
 import org.bukkit.inventory.InventoryHolder;
 import org.bukkit.material.Sign;
 
-import de.cuuky.cfw.version.types.Materials;
 import de.cuuky.cfw.version.types.Sounds;
 import de.cuuky.varo.Main;
 import de.cuuky.varo.configuration.configurations.config.ConfigSetting;
@@ -27,19 +26,24 @@ import de.cuuky.varo.entity.player.stats.stat.inventory.VaroSaveable.SaveableTyp
 public class SignChangeListener implements Listener {
 
 	@EventHandler(priority = EventPriority.HIGHEST)
-	public void onSignChange(SignChangeEvent e) {
-		Sign sign = (Sign) e.getBlock().getState().getData();
-		if (e.getPlayer().isOp())
-			for (int i = 0; i < e.getLines().length; i++)
-				e.setLine(i, e.getLines()[i].replace("&", "§"));
-
-		if (e.getBlock().getType() != Materials.SIGN.parseMaterial() && e.getBlock().getType() != Materials.WALL_SIGN.parseMaterial())
+	public void onSignChange(SignChangeEvent event) {
+		Sign sign = null;
+		
+		try {
+			sign = (Sign) event.getBlock().getState().getData();
+		} catch (ClassCastException e) {
+			event.getPlayer().sendMessage(Main.getPrefix() + "§cNot available on your MC-Version!");
 			return;
+		}
+		
+		if (event.getPlayer().isOp())
+			for (int i = 0; i < event.getLines().length; i++)
+				event.setLine(i, event.getLines()[i].replace("&", "§"));
 
 		if (!Main.getVaroGame().hasStarted())
 			return;
 
-		Block attached = e.getBlock().getRelative(sign.getAttachedFace());
+		Block attached = event.getBlock().getRelative(sign.getAttachedFace());
 
 		if (attached.getState() instanceof Chest) {
 			Chest chest = (Chest) attached.getState();
@@ -49,11 +53,11 @@ public class SignChangeListener implements Listener {
 				secChest = (Chest) ((DoubleChest) ih).getRightSide();
 
 			if (ConfigSetting.PLAYER_CHEST_LIMIT.getValueAsInt() == 0) {
-				e.getPlayer().sendMessage(Main.getPrefix() + "§7Die Chestsicherung wurde in der Config §7deaktiviert!");
+				event.getPlayer().sendMessage(Main.getPrefix() + "§7Die Chestsicherung wurde in der Config §7deaktiviert!");
 				return;
 			}
 
-			Player p = e.getPlayer();
+			Player p = event.getPlayer();
 			VaroPlayer player = VaroPlayer.getPlayer(p);
 			ArrayList<VaroSaveable> teamSaves = VaroSaveable.getSaveable(player);
 			ArrayList<VaroSaveable> sorted = new ArrayList<>();
@@ -66,23 +70,23 @@ public class SignChangeListener implements Listener {
 
 			if (sorted.size() >= ConfigSetting.PLAYER_CHEST_LIMIT.getValueAsInt() || secChest != null && sorted.size() + 1 >= ConfigSetting.PLAYER_CHEST_LIMIT.getValueAsInt()) {
 				p.sendMessage(Main.getPrefix() + "§7Die maximale Anzahl an gesetzten Kisten fuer " + Main.getColorCode() + (player.getTeam() != null ? "das Team " + player.getTeam().getName() : "dich") + " §7wurde bereits §7erreicht! (Anzahl: §6" + sorted.size() + " §7Max: §6" + ConfigSetting.PLAYER_CHEST_LIMIT.getValueAsInt() + "§7)");
-				e.setCancelled(true);
+				event.setCancelled(true);
 				return;
 			}
 
 			if (VaroSaveable.getByLocation(chest.getLocation()) != null || secChest != null && VaroSaveable.getByLocation(secChest.getLocation()) != null) {
 				p.sendMessage(Main.getPrefix() + "§7Diese " + Main.getColorCode() + " Kiste §7ist bereits gesichert!");
-				e.setCancelled(true);
+				event.setCancelled(true);
 				return;
 			}
 
 			if (secChest != null)
 				new VaroSaveable(SaveableType.CHEST, secChest.getLocation(), player);
 
-			e.setLine(0, "§8--------------");
-			e.setLine(1, "§lSavedChest");
-			e.setLine(2, Main.getColorCode() + (player.getTeam() != null ? player.getTeam().getDisplay() : player.getName()));
-			e.setLine(3, "§8--------------");
+			event.setLine(0, "§8--------------");
+			event.setLine(1, "§lSavedChest");
+			event.setLine(2, Main.getColorCode() + (player.getTeam() != null ? player.getTeam().getDisplay() : player.getName()));
+			event.setLine(3, "§8--------------");
 			p.playSound(p.getLocation(), Sounds.NOTE_PLING.bukkitSound(), 1, 1);
 			for (int i = 0; i < 6; i++)
 				p.getWorld().playEffect(chest.getLocation(), Effect.ENDER_SIGNAL, 1);
@@ -93,11 +97,11 @@ public class SignChangeListener implements Listener {
 			Furnace furnace = (Furnace) attached.getState();
 
 			if (ConfigSetting.PLAYER_FURNACE_LIMIT.getValueAsInt() == 0) {
-				e.getPlayer().sendMessage(Main.getPrefix() + "§7Die Furnacesicherung wurde in der Config §7deaktiviert!");
+				event.getPlayer().sendMessage(Main.getPrefix() + "§7Die Furnacesicherung wurde in der Config §7deaktiviert!");
 				return;
 			}
 
-			Player p = e.getPlayer();
+			Player p = event.getPlayer();
 			VaroPlayer player = VaroPlayer.getPlayer(p);
 
 			ArrayList<VaroSaveable> teamSaves = VaroSaveable.getSaveable(player);
@@ -111,21 +115,21 @@ public class SignChangeListener implements Listener {
 
 			if (VaroSaveable.getByLocation(furnace.getLocation()) != null) {
 				p.sendMessage(Main.getPrefix() + "§7Diese " + Main.getColorCode() + " Furnace §7ist bereits gesichert!");
-				e.setCancelled(true);
+				event.setCancelled(true);
 				return;
 			}
 
 			if (ConfigSetting.PLAYER_FURNACE_LIMIT.isIntActivated())
 				if (sorted.size() >= ConfigSetting.PLAYER_FURNACE_LIMIT.getValueAsInt()) {
 					p.sendMessage(Main.getPrefix() + "§7Die maximale Anzahl an gesetzten Oefen fuer " + Main.getColorCode() + (player.getTeam() != null ? "das Team " + player.getTeam().getName() : "dich") + " §7wurde bereits §7erreicht! (Anzahl: §6" + sorted.size() + " §7Max: §6" + ConfigSetting.PLAYER_CHEST_LIMIT.getValueAsInt() + "§7)");
-					e.setCancelled(true);
+					event.setCancelled(true);
 					return;
 				}
 
-			e.setLine(0, "§8--------------");
-			e.setLine(1, "§lSavedFurnace");
-			e.setLine(2, Main.getColorCode() + (player.getTeam() != null ? player.getTeam().getDisplay() : player.getName()));
-			e.setLine(3, "§8--------------");
+			event.setLine(0, "§8--------------");
+			event.setLine(1, "§lSavedFurnace");
+			event.setLine(2, Main.getColorCode() + (player.getTeam() != null ? player.getTeam().getDisplay() : player.getName()));
+			event.setLine(3, "§8--------------");
 			p.playSound(furnace.getLocation(), Sounds.NOTE_PLING.bukkitSound(), 1, 1);
 			for (int i = 0; i < 6; i++)
 				p.getWorld().playEffect(furnace.getLocation(), Effect.ENDER_SIGNAL, 1);
