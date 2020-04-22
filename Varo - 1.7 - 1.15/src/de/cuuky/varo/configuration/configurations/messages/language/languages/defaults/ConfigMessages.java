3e8cfdf36b0986c7613e2ba14f435df9f250e16a
CuukyOfficial <just.cookie.jc@gmail.com>
2020-04-22 19:30:42 +0200

    Corrected typo

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/configuration/configurations/messages/language/languages/defaults/ConfigMessages.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/configuration/configurations/messages/language/languages/defaults/ConfigMessages.java
@@ -230,7 +230,7 @@ public enum ConfigMessages implements DefaultLanguage {
 	VARO_COMMANDS_CONFIG_RELOADED("varoCommands.config.reloaded", "&7Alle %colorcode%Listen&7, %colorcode%Nachrichten &7und die %colorcode%Config &7wurden erfolgreich neu geladen."),
 	VARO_COMMANDS_CONFIG_HELP_SET("varoCommands.config.helpset", "%colorcode%/varo config &7set <key> <value>"),
 	VARO_COMMANDS_CONFIG_HELP_SEARCH("varoCommands.config.helpsearch", "%colorcode%/varo config &7search <key>"),
-	VARO_COMMANDS_CONFIG_ENTRY_SET("varoCommands.config.entryset", "&7Der Eintrag '%colorcode%%entry%&7' wurde erfolgreich auf '%colorcode%%value%&7'gesetzt."),
+	VARO_COMMANDS_CONFIG_ENTRY_SET("varoCommands.config.entryset", "&7Der Eintrag '%colorcode%%entry%&7' wurde erfolgreich auf '%colorcode%%value%&7' gesetzt."),
 	VARO_COMMANDS_CONFIG_ENTRY_NOT_FOUND("varoCommands.config.entrynotfound", "&7Der Eintrag '%colorcode%%entry%&7' wurde nicht gefunden."),
 	VARO_COMMANDS_CONFIG_RESET("varoCommands.config.reset", "&7Alle Eintraege wurden erfolgreich zurueckgesetzt."),
 	VARO_COMMANDS_CONFIG_SEARCH_LIST_TITLE("varoCommands.config.searchlisttitle", "&lFolgende Einstellungen wurden gefunden:"),
