ee8b3a4fdd91c5cbc0da1fb0e330e061757be2f4
CuukyOfficial <just.cookie.jc@gmail.com>
2020-04-25 10:13:49 +0200

    Removed command blocker

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/listener/PlayerCommandPreprocessListener.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/listener/PlayerCommandPreprocessListener.java
@@ -11,17 +11,13 @@ public class PlayerCommandPreprocessListener implements Listener {
 	@EventHandler
 	public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
 		String lowerMessage = event.getMessage().toLowerCase();
-		if (lowerMessage.startsWith("/pl") || lowerMessage.startsWith("/plugins") || lowerMessage.startsWith("/?") || lowerMessage.startsWith("/bukkit:?") || lowerMessage.startsWith("/bukkit:pl") || lowerMessage.startsWith("/bukkit:plugins") || lowerMessage.startsWith("/help") || lowerMessage.startsWith("/bukkit:help") || lowerMessage.startsWith("/me") || lowerMessage.startsWith("/minecraft:me") || lowerMessage.startsWith("/tell") || lowerMessage.startsWith("/minecraft:tell") || lowerMessage.startsWith("/icanhasbukkit") || lowerMessage.startsWith("/version") || lowerMessage.startsWith("/bukkit:version") || lowerMessage.startsWith("/ver") || lowerMessage.startsWith("/about") || lowerMessage.startsWith("/bukkit:about")) {
 
-			if (event.getPlayer().hasPermission("varo.readInfo"))
-				return;
+		if (!lowerMessage.startsWith("/tell") && !lowerMessage.startsWith("/bukkit:tell"))
+			return;
+		if (Main.getVaroGame().isRunning())
+			return;
 
-			if (Main.getVaroGame().isRunning())
-				if (lowerMessage.contains("tell"))
-					return;
-
-			event.setCancelled(true);
-			event.getPlayer().sendMessage(Main.getPrefix() + "§7Nein.");
-		}
+		event.setCancelled(true);
+		event.getPlayer().sendMessage(Main.getPrefix() + "§7Nein.");
 	}
 }
