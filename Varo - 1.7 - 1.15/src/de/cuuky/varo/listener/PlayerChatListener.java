73b1a812107f203a2483dfcf480437430e231023
CuukyOfficial <just.cookie.jc@gmail.com>
2020-04-30 17:37:07 +0200

    Uncalled shit

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/listener/PlayerChatListener.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/listener/PlayerChatListener.java
@@ -21,7 +21,7 @@ public class PlayerChatListener implements Listener {
 
 	private void sendMessageToAll(String msg, VaroPlayer vp, AsyncPlayerChatEvent event) {
 		if (vp.getStats().getYoutubeLink() == null) {
-			event.setCancelled(true);
+			event.setCancelled(false);
 			event.setFormat(msg);
 			return;
 		}
