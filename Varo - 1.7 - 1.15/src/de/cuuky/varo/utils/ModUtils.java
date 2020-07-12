8fd253230e33615e52a39496c2cd8d029ba4dee4
CuukyOfficial <just.cookie.jc@gmail.com>
2020-07-12 14:07:11 +0200

    Made magma access over reflections

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/utils/ModUtils.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/utils/ModUtils.java
@@ -1,10 +1,11 @@
 package de.cuuky.varo.utils;
 
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.ArrayList;
 
 import org.bukkit.Bukkit;
 import org.bukkit.entity.Player;
-import org.magmafoundation.magma.api.PlayerAPI;
 
 import de.cuuky.cfw.utils.JavaUtils;
 import de.cuuky.varo.Main;
@@ -13,26 +14,42 @@ import de.cuuky.varo.entity.player.VaroPlayer;
 
 public class ModUtils {
 
+	private static Method getModList;
+
+	static {
+		try {
+			Class<?> playerApiClass = Class.forName("org.magmafoundation.magma.api.PlayerAPI");
+			getModList = playerApiClass.getMethod("getModlist", Player.class);
+		} catch (NoSuchMethodException | ClassNotFoundException e) {}
+	}
+
 	public static void checkForIllegalMods(Player player) {
+		if (getModList == null)
+			return;
 
 		boolean kickPlayer = false;
-		String modList = PlayerAPI.getModlist(player);
+		String modList = null;
+		try {
+			modList = (String) getModList.invoke(null, player);
+		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			return;
+		}
+
 		ArrayList<String> usedMods = new ArrayList<String>();
-		for(String mod : Main.getDataManager().getListManager().getBlockedMods().getAsList()) {
-			if(modList.toLowerCase().contains(mod.toLowerCase())) {
+		for (String mod : Main.getDataManager().getListManager().getBlockedMods().getAsList()) {
+			if (modList.toLowerCase().contains(mod.toLowerCase())) {
 				kickPlayer = true;
 				usedMods.add(mod);
 			}
-			if(kickPlayer) {
+			if (kickPlayer) {
 				String illegalMods = JavaUtils.getArgsToString(usedMods, "§7, " + Main.getColorCode());
-				if(!player.hasPermission("varo.alwaysjoin")) {
+				if (!player.hasPermission("varo.alwaysjoin")) {
 					player.kickPlayer(Main.getPrefix() + ConfigMessages.MODS_BLOCKED_MODS_KICK.getValue(VaroPlayer.getPlayer(player)).replace("%mods%", illegalMods));
 				}
-				for(Player p : Bukkit.getOnlinePlayers()) {
+				for (Player p : Bukkit.getOnlinePlayers()) {
 					VaroPlayer.getPlayer(p).sendMessage(Main.getPrefix() + ConfigMessages.MODS_BLOCKED_MODS_BROADCAST.getValue().replace("%mods%", illegalMods).replace("%player%", player.getName()));
 				}
 			}
 		}
-
 	}
 }
