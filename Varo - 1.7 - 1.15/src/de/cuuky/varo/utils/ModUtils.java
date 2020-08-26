ac546b74457e6b5173095ceea898e312fb368e25
UeberallGebannt <paulmertens63@gmail.com>
2020-08-26 08:16:04 +0200

    Fix getModListString

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/utils/ModUtils.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/utils/ModUtils.java
@@ -22,11 +22,11 @@ public class ModUtils {
 		try {
 			Class<?> playerApiClass = Class.forName("org.magmafoundation.magma.api.PlayerAPI");
 			getModList = playerApiClass.getMethod("getModlist", Player.class);
-		} catch (NoSuchMethodException | ClassNotFoundException e) {}
+		} catch(NoSuchMethodException | ClassNotFoundException e) {}
 	}
 
 	public static void checkForIllegalMods(Player player) {
-		if (getModList == null)
+		if(getModList == null)
 			return;
 
 		boolean kickPlayer = false;
@@ -34,31 +34,32 @@ public class ModUtils {
 		modList = getModListString(player);
 
 		ArrayList<String> usedMods = new ArrayList<String>();
-		for (String mod : Main.getDataManager().getListManager().getBlockedMods().getAsList()) {
-			if (modList.toLowerCase().contains(mod.toLowerCase())) {
+		for(String mod : Main.getDataManager().getListManager().getBlockedMods().getAsList()) {
+			if(modList.toLowerCase().contains(mod.toLowerCase())) {
 				kickPlayer = true;
 				usedMods.add(mod);
 			}
-			if (kickPlayer) {
+			if(kickPlayer) {
 				String illegalMods = JavaUtils.getArgsToString(usedMods, ConfigMessages.MODS_BLOCKED_MODLIST_SPLIT.getValue() + Main.getColorCode());
-				if (!player.hasPermission("varo.alwaysjoin")) {
+				if(!player.hasPermission("varo.alwaysjoin")) {
 					player.kickPlayer(Main.getPrefix() + ConfigMessages.MODS_BLOCKED_MODS_KICK.getValue(VaroPlayer.getPlayer(player)).replace("%mods%", illegalMods));
 				}
-				for (Player p : Bukkit.getOnlinePlayers()) {
+				for(Player p : Bukkit.getOnlinePlayers()) {
 					VaroPlayer.getPlayer(p).sendMessage(Main.getPrefix() + ConfigMessages.MODS_BLOCKED_MODS_BROADCAST.getValue().replace("%mods%", illegalMods).replace("%player%", player.getName()));
 				}
 			}
 		}
 	}
-	
+
 	public static List<String> getModList(Player player) {
 		return Arrays.asList(getModListString(player).split(", "));
 	}
-	
+
 	public static String getModListString(Player player) {
 		try {
-			return (String) getModList.invoke(null, player);
-		} catch (IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
+			String mods = (String) getModList.invoke(null, player);
+			return mods.equalsIgnoreCase("null") ? "-" : mods;
+		} catch(IllegalAccessException | IllegalArgumentException | InvocationTargetException e) {
 			return "-";
 		}
 	}
