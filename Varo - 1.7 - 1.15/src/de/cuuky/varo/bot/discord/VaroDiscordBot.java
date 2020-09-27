3111747f3d1d70b91792c730e540c5fa167c28c4
UeberallGebannt <50475262+UeberallGebannt@users.noreply.github.com>
2020-09-27 10:56:44 +0200

    Fix Embeds

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/bot/discord/VaroDiscordBot.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/bot/discord/VaroDiscordBot.java
@@ -136,7 +136,7 @@ public class VaroDiscordBot implements VaroBot {
 			System.err.println(Main.getConsolePrefix() + "Failed to print discord message!");
 			return;
 		}
-		if (ConfigSetting.DISCORDBOT_MESSAGE_RANDOM_COLOR.getValueAsBoolean()) {
+		if (ConfigSetting.DISCORDBOT_USE_EMBEDS.getValueAsBoolean()) {
 			EmbedBuilder builder = new EmbedBuilder();
 			if (!ConfigSetting.DISCORDBOT_MESSAGE_RANDOM_COLOR.getValueAsBoolean())
 				builder.setColor(color);
