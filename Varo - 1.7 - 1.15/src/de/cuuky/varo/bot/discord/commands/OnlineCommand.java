fc36c3281ef8fd0fb7701b6d81a5de7c84cb5260
UeberallGebannt <paulmertens63@gmail.com>
2020-12-26 17:15:06 +0100

    Mark spectators in Discord online command

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/bot/discord/commands/OnlineCommand.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/bot/discord/commands/OnlineCommand.java
@@ -4,6 +4,7 @@ import java.awt.Color;
 
 import de.cuuky.varo.bot.discord.DiscordBotCommand;
 import de.cuuky.varo.entity.player.VaroPlayer;
+import de.cuuky.varo.entity.player.stats.stat.PlayerState;
 import net.dv8tion.jda.api.events.message.MessageReceivedEvent;
 
 public class OnlineCommand extends DiscordBotCommand {
@@ -25,10 +26,11 @@ public class OnlineCommand extends DiscordBotCommand {
 
 		String players = "";
 		for (VaroPlayer vp : VaroPlayer.getOnlinePlayer()) {
+			String name = vp.getStats().getState() == PlayerState.SPECTATOR ? vp.getName() + " (SPECTATOR)" : vp.getName();
 			if (players.equals(""))
-				players = vp.getName();
+				players = name;
 			else
-				players = players + ", " + vp.getName();
+				players = players + ", " + name;
 		}
 
 		getDiscordBot().sendRawMessage("ONLINE (" + VaroPlayer.getOnlinePlayer().size() + ") \n\n" + players, event.getTextChannel());
