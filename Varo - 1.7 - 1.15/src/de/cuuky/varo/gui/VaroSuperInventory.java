1eff51364e1b66499932b1de7a135626061984a4
CuukyOfficial <just.cookie.jc@gmail.com>
2021-04-08 11:57:08 +0200

    Replaced outdated discordbot link

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/gui/VaroSuperInventory.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/gui/VaroSuperInventory.java
@@ -1,12 +1,17 @@
 package de.cuuky.varo.gui;
 
+import de.cuuky.varo.configuration.configurations.config.ConfigSetting;
 import org.bukkit.entity.Player;
 
 import de.cuuky.cfw.menu.SuperInventory;
+import sun.security.krb5.internal.ccache.CredentialsCache;
 
 public abstract class VaroSuperInventory extends SuperInventory {
 
 	public VaroSuperInventory(String title, Player opener, int size, boolean homePage) {
 		super(title.length() > 32 ? title.substring(0, 32) : title, opener, size, homePage);
+
+		this.animations = ConfigSetting.GUI_INVENTORY_ANIMATIONS.getValueAsBoolean();
+		this.fillInventory = ConfigSetting.GUI_FILL_INVENTORY.getValueAsBoolean();
 	}
 }
