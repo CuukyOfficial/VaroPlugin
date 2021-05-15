e426bb61b004254ae2779194fdf299120877de4b
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2021-05-15 13:14:17 +0200

    fixed worldedit crash

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/listener/PlayerCommandPreprocessListener.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/listener/PlayerCommandPreprocessListener.java
@@ -1,5 +1,9 @@
 package de.cuuky.varo.listener;
 
+import java.util.Arrays;
+import java.util.List;
+
+import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerCommandPreprocessEvent;
@@ -7,17 +11,20 @@ import org.bukkit.event.player.PlayerCommandPreprocessEvent;
 import de.cuuky.varo.Main;
 
 public class PlayerCommandPreprocessListener implements Listener {
+	
+	private static final List<String> WORLDEDIT_CRASH_COMMANDS = Arrays.asList(new String[] {"//calc", "/worldedit:/calc", "//calculate", "/worldedit:/calculate", "//eval", "/worldedit:/eval", "//evaluate", "/worldedit:/evaluate", "//solve", "/worldedit:/solve"});
+	private static final List<String> TELL_COMMANDS = Arrays.asList(new String[] {"/tell", "/bukkit:tell", "/me", "/bukkit:me"});
 
 	@EventHandler
 	public void onPlayerCommand(PlayerCommandPreprocessEvent event) {
-		String lowerMessage = event.getMessage().toLowerCase();
-		if (!lowerMessage.startsWith("/tell") && !lowerMessage.startsWith("/bukkit:tell") && !lowerMessage.startsWith("/me") && !lowerMessage.startsWith("/bukkit:me"))
-			return;
-
-		if (Main.getVaroGame().isRunning())
-			return;
-
-		event.setCancelled(true);
-		event.getPlayer().sendMessage(Main.getPrefix() + "§7Nein.");
+		String lowerMessage = event.getMessage().split(" ", 2)[0].toLowerCase();
+		
+		if (WORLDEDIT_CRASH_COMMANDS.contains(lowerMessage)) {
+			event.setCancelled(true);
+			Bukkit.getServer().broadcastMessage(String.format("%s§e%s §chat möglicherweise versucht den Server zu crashen!", Main.getPrefix(), event.getPlayer().getName(), event.getMessage()));
+		} else if (TELL_COMMANDS.contains(lowerMessage) && !Main.getVaroGame().isRunning()) {
+			event.setCancelled(true);
+			event.getPlayer().sendMessage(Main.getPrefix() + "§7Nein.");
+		}
 	}
 }
