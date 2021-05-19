2edd7cb4b71f6081829c5bfbd3e9df90d2f7bcc1
CuukyOfficial <just.cookie.jc@gmail.com>
2021-05-19 17:32:14 +0200

    Reworked stats command

--- Varo - 1.7 - 1.15/src/de/cuuky/varo/command/varo/StatsCommand.java
+++ Varo - 1.7 - 1.15/src/de/cuuky/varo/command/varo/StatsCommand.java
@@ -1,243 +1,213 @@
 package de.cuuky.varo.command.varo;
 
-import org.bukkit.command.Command;
-import org.bukkit.command.CommandSender;
-
 import de.cuuky.varo.Main;
 import de.cuuky.varo.command.VaroCommand;
 import de.cuuky.varo.configuration.configurations.config.ConfigSetting;
 import de.cuuky.varo.entity.player.VaroPlayer;
 import de.cuuky.varo.entity.player.stats.stat.PlayerState;
 import de.cuuky.varo.entity.player.stats.stat.Rank;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.function.Consumer;
+import java.util.function.Function;
+import java.util.stream.Collectors;
 
 public class StatsCommand extends VaroCommand {
 
-	public enum SetArgumentType {
+    interface ArgumentReceiver {
 
-		ADMIN_IGNORE("adminignore"),
-		COUNTDOWN("countdown"),
-		EPISODES_PLAYED("episodesplayed"),
-		KILLS("kills"),
-		PLAYER_STATE("playerstate"),
-		RANK("rank"),
-		SESSIONS("sessions"),
-		WINS("wins"),
-		YOUTUBE_LINK("youtubelink");
+        void receive(String value, VaroPlayer vp);
 
-		private String arg;
+    }
 
-		private SetArgumentType(String arg) {
-			this.arg = arg;
-		}
+    public enum SetArgumentType {
 
-		public boolean execute(String value, VaroPlayer vp, CommandSender sender) {
-			try {
-				switch (this) {
-				case ADMIN_IGNORE:
-					vp.setAdminIgnore(Boolean.valueOf(value));
-					break;
-				case KILLS:
-					vp.getStats().setKills(Integer.parseInt(value));
-					break;
-				case SESSIONS:
-					vp.getStats().setSessions(Integer.parseInt(value));
-					break;
-				case EPISODES_PLAYED:
-					vp.getStats().setSessionsPlayed(Integer.parseInt(value));
-					break;
-				case RANK:
-					vp.setRank(new Rank(value));
-					break;
-				case COUNTDOWN:
-					vp.getStats().setCountdown(Integer.parseInt(value));
-					break;
-				case PLAYER_STATE:
-					vp.getStats().setState(PlayerState.getByName(value));
-					break;
-				case WINS:
-					vp.getStats().setWins(Integer.parseInt(value));
-					break;
-				case YOUTUBE_LINK:
-					vp.getStats().setYoutubeLink(value);
-					break;
-				}
+        ADMIN_IGNORE("adminignore", (value, vp) -> vp.setAdminIgnore(Boolean.parseBoolean(value)), VaroPlayer::isAdminIgnore, (vp) -> vp.setAdminIgnore(false)),
+        COUNTDOWN("countdown", (value, vp) -> vp.getStats().setCountdown(Integer.parseInt(value)), vp -> vp.getStats().getCountdown(), (vp) -> vp.getStats().setCountdown(ConfigSetting.PLAY_TIME.getValueAsInt() * 60)),
+        EPISODES_PLAYED("episodesplayed", (value, vp) -> vp.getStats().setSessionsPlayed(Integer.parseInt(value)), vp -> vp.getStats().getSessionsPlayed(), (vp) -> vp.getStats().setSessionsPlayed(0)),
+        KILLS("kills", (value, vp) -> vp.getStats().setKills(Integer.parseInt(value)), vp -> vp.getStats().getKills(), (vp) -> vp.getStats().setKills(0)),
+        PLAYER_STATE("playerstate", (value, vp) -> vp.getStats().setState(PlayerState.getByName(value)), vp -> vp.getStats().getState(), (vp) -> vp.getStats().setState(PlayerState.ALIVE)),
+        RANK("rank", (value, vp) -> vp.setRank(new Rank(value)), vp -> vp.getRank().getDisplay(), (vp) -> vp.setRank(null)),
+        SESSIONS("sessions", (value, vp) -> vp.getStats().setSessions(Integer.parseInt(value)), vp -> vp.getStats().getSessions(), (vp) -> vp.getStats().setSessions(ConfigSetting.SESSIONS_PER_DAY.getValueAsInt())),
+        WINS("wins", (value, vp) -> vp.getStats().setWins(Integer.parseInt(value)), vp -> vp.getStats().getWins(), (vp) -> vp.getStats().setWins(0)),
+        YOUTUBE_LINK("youtubelink", (value, vp) -> vp.getStats().setYoutubeLink(value), vp -> vp.getStats().getYoutubeLink(), (vp) -> vp.getStats().setYoutubeLink(null));
 
-				if (vp.isOnline())
-					vp.update();
-			} catch (Exception e) {
-				e.printStackTrace();
-				sender.sendMessage(Main.getPrefix() + "§7Der Wert '" + Main.getColorCode() + value + "§7' §7konnte nicht fuer " + this.toString() + " gesetzt werden!");
-				return false;
-			}
-			return true;
-		}
+        private final String arg;
+        private final ArgumentReceiver receiver;
+        private final Function<VaroPlayer, Object> getter;
+        private final Consumer<VaroPlayer> reset;
 
-		public String getArg() {
-			return this.arg;
-		}
+        SetArgumentType(String arg, ArgumentReceiver receiver, Function<VaroPlayer, Object> getter, Consumer<VaroPlayer> reset) {
+            this.arg = arg;
+            this.receiver = receiver;
+            this.getter = getter;
+            this.reset = reset;
+        }
 
-		public void remove(VaroPlayer vp) {
-			switch (this) {
-			case ADMIN_IGNORE:
-				vp.setAdminIgnore(false);
-				break;
-			case KILLS:
-				vp.getStats().setKills(0);
-				break;
-			case SESSIONS:
-				vp.getStats().setSessions(ConfigSetting.SESSIONS_PER_DAY.getValueAsInt());
-				break;
-			case EPISODES_PLAYED:
-				vp.getStats().setSessionsPlayed(0);
-				break;
-			case RANK:
-				vp.setRank(null);
-				break;
-			case COUNTDOWN:
-				vp.getStats().setCountdown(ConfigSetting.PLAY_TIME.getValueAsInt() * 60);
-				break;
-			case PLAYER_STATE:
-				vp.getStats().setState(PlayerState.ALIVE);
-				break;
-			case WINS:
-				vp.getStats().setWins(0);
-				break;
-			case YOUTUBE_LINK:
-				vp.getStats().setYoutubeLink(null);
-				break;
-			}
+        public void execute(String value, VaroPlayer vp, CommandSender sender) {
+            try {
+                this.receiver.receive(value, vp);
+                if (vp.isOnline())
+                    vp.update();
+            } catch (Exception e) {
+                e.printStackTrace();
+                sender.sendMessage(Main.getPrefix() + "§7Der Wert '" + Main.getColorCode() + value + "§7' §7konnte nicht fuer " + this.toString() + " gesetzt werden!");
+            }
+        }
 
-			if (vp.isOnline())
-				vp.update();
-		}
+        public void remove(VaroPlayer vp) {
+            this.reset.accept(vp);
+            if (vp.isOnline())
+                vp.update();
+        }
 
-		public static SetArgumentType getByName(String name) {
-			for (SetArgumentType type : values())
-				if (type.getArg().equalsIgnoreCase(name))
-					return type;
+        public String getArg() {
+            return this.arg;
+        }
 
-			return null;
-		}
-	}
+        public Object get(VaroPlayer vp) {
+            return this.getter.apply(vp);
+        }
 
-	public StatsCommand() {
-		super("stats", "Bearbeiten von Stats", "varo.stats", "stat");
-	}
+        public static SetArgumentType getByName(String name) {
+            for (SetArgumentType type : values())
+                if (type.getArg().equalsIgnoreCase(name))
+                    return type;
 
-	@Override
-	public void onCommand(CommandSender sender, VaroPlayer vp, Command cmd, String label, String[] args) {
-		if (args.length == 0) {
-			sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats §7<Spieler/@a>");
-			sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats set §7<Spieler/@a> <Stat> <Value>");
-			sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats remove §7<Spieler/@a> <Stat>");
-			sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats reset §7<Spieler/@a>");
-			sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats odvReset §7<Spieler/@a> | Resettet alles ausser Kills, Wins, Rank, Team und YT-Link");
-			return;
-		}
+            return null;
+        }
+    }
 
-		String t = args.length == 1 ? args[0] : args[1];
-		VaroPlayer target = VaroPlayer.getPlayer(t);
-		if (target == null && !t.equalsIgnoreCase("@a")) {
-			sender.sendMessage(Main.getPrefix() + Main.getColorCode() + t + " §7nicht gefunden!");
-			return;
-		}
+    public StatsCommand() {
+        super("stats", "Bearbeiten von Stats", "varo.stats", "stat");
+    }
 
-		if (args.length == 1) {
-			if (t.equalsIgnoreCase("@a")) {
-				sender.sendMessage(Main.getPrefix() + "Du kannst nur Stats von einer Person gleichzeitig auslesen!");
-				return;
-			}
+    private String doCommand(Function<VaroPlayer, Boolean> execute, String[] args, int start) {
+        List<VaroPlayer> players = new ArrayList<>();
+        if (args[start].equals("@a"))
+            players.addAll(VaroPlayer.getVaroPlayer());
+        else {
+            for (int i = start; i < args.length; i++) {
+                VaroPlayer vp = VaroPlayer.getPlayer(args[i]);
+                if (vp == null)
+                    continue;
 
-			sender.sendMessage(Main.getPrefix() + "Stats von " + Main.getColorCode() + target.getName() + "§7:");
-			sender.sendMessage(Main.getPrefix());
-			for (String stat : target.getStats().getStatsListed())
-				sender.sendMessage(Main.getPrefix() + stat);
-			return;
-		}
+                players.add(VaroPlayer.getPlayer(args[i]));
+            }
+        }
 
-		if (args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("remove")) {
-			if (args.length == 2) {
-				for (SetArgumentType all : SetArgumentType.values())
-					sender.sendMessage(Main.getPrefix() + "STAT: " + Main.getColorCode() + all.getArg());
-				return;
-			}
+        if (players.isEmpty())
+            return null;
 
-			SetArgumentType type = SetArgumentType.getByName(args[2]);
-			if (args[0].equalsIgnoreCase("set")) {
-				if (args.length != 4) {
-					sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats set §7<Spieler/@a> <Stat> <Value>");
-					return;
-				}
-			} else {
-				if (args.length != 3) {
-					sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats remove §7<Spieler/@a> <Stat>");
-					return;
-				}
-			}
+        for (VaroPlayer vp : players)
+            if (!execute.apply(vp))
+                return null;
 
-			if (type == null) {
-				sender.sendMessage(Main.getPrefix() + "Stat '" + Main.getColorCode() + args[2] + "§7' konnte nicht gefunden werden!");
-				return;
-			}
+        return players.stream().map(VaroPlayer::getName).collect(Collectors.joining(", "));
+    }
 
-			if (target == null) {
-				if (args[0].equalsIgnoreCase("set")) {
-					for (VaroPlayer all : VaroPlayer.getVaroPlayer())
-						if (!type.execute(args[3], all, sender)) {
-							sender.sendMessage(Main.getPrefix() + "Vorgang fuer alle Spieler abgebrochen!");
-							return;
-						}
+    @Override
+    public void onCommand(CommandSender sender, VaroPlayer vp, Command cmd, String label, String[] args) {
+        if (args.length == 0) {
+            sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats §7<Spieler>");
+            sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats set/add*/decrease* §7<Stat> <Value> <Spieler.../@a> | Setzt einen Stat auf einen bestimmten Wert");
+            sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats remove §7<Stat> <Spieler.../@a> | Setzt einen oder alle Wert(e) zurück");
+            sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats reset §7<Spieler.../@a> | Resettet alle Stats");
+            sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats odvReset §7<Spieler.../@a> | Resettet alles ausser Kills, Wins, Rank, Team und YT-Link");
+            sender.sendMessage(Main.getPrefix() + Main.getColorCode() + " *Nur für Werte nutzbar, die eine Zahl sind");
+            sender.sendMessage(Main.getPrefix() + " Tipp: Das '...' bedeutet, dass man mehrere Spieler angeben kann.");
+            return;
+        }
 
-					sender.sendMessage(Main.getPrefix() + "Stat '" + Main.getColorCode() + type.getArg() + "§7' fuer alle Spieler erfolgreich auf '" + Main.getColorCode() + args[3] + "§7' gesetzt!");
-				} else {
-					for (VaroPlayer all : VaroPlayer.getVaroPlayer())
-						type.remove(all);
+        if (args.length == 1) {
+            VaroPlayer target;
+            if ((target = VaroPlayer.getPlayer(args[0])) == null) {
+                sender.sendMessage(Main.getPrefix() + "Spieler nicht gefunden!");
+                return;
+            }
 
-					sender.sendMessage(Main.getPrefix() + "Stat '" + Main.getColorCode() + type.getArg() + "§7' fuer alle Spieler erfolgreich zurueckgesetzt/entfernt!");
-				}
+            sender.sendMessage(Main.getPrefix() + "Stats von " + Main.getColorCode() + target.getName() + "§7:");
+            sender.sendMessage(Main.getPrefix());
+            for (String stat : target.getStats().getStatsListed())
+                sender.sendMessage(Main.getPrefix() + stat);
+            return;
+        }
 
-				return;
-			}
+        if (args[0].equalsIgnoreCase("set") || args[0].equalsIgnoreCase("remove") || args[0].equalsIgnoreCase("add") || args[0].equalsIgnoreCase("decrease")) {
+            if (args.length == 2) {
+                String stats = Arrays.stream(SetArgumentType.values()).map(SetArgumentType::getArg).collect(Collectors.joining(", "));
+                sender.sendMessage(Main.getPrefix() + "Stats available: " + stats);
+                return;
+            }
 
-			if (args[0].equalsIgnoreCase("set")) {
-				if (type.execute(args[3], target, sender))
-					sender.sendMessage(Main.getPrefix() + "Stat '" + Main.getColorCode() + type.getArg() + "§7' fuer '" + Main.getColorCode() + target.getName() + "§7' erfolgreich auf '" + Main.getColorCode() + args[3] + "§7' gesetzt!");
-			} else {
-				type.remove(target);
-				sender.sendMessage(Main.getPrefix() + "Stat '" + Main.getColorCode() + type.getArg() + "§7' fuer '" + Main.getColorCode() + target.getName() + "§7' erfolgreich zurueckgesetzt/entfernt!");
-			}
-		} else if (args[0].equalsIgnoreCase("reset")) {
-			if (target == null) {
-				for (VaroPlayer all : VaroPlayer.getVaroPlayer())
-					all.getStats().loadDefaults();
+            SetArgumentType type = SetArgumentType.getByName(args[1]);
+            boolean set = !args[0].equalsIgnoreCase("remove");
+            int min = set ? 4 : 3;
+            if (set) {
+                if (args.length < min) {
+                    sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats set/add/decrease §7<Stat...> <Value> <Spieler.../@a> | Setzt einen Stat auf einen bestimmten Wert");
+                    return;
+                }
+            } else {
+                if (args.length < min) {
+                    sender.sendMessage(Main.getPrefix() + Main.getColorCode() + label + " stats remove §7<Stat> <Spieler.../@a> | Setzt einen oder alle Wert(e) zurück");
+                    return;
+                }
+            }
 
-				sender.sendMessage(Main.getPrefix() + "Alle Spieler erfolgreich zurueckgesetzt!");
-				return;
-			}
+            if (type == null) {
+                sender.sendMessage(Main.getPrefix() + "Stat '" + Main.getColorCode() + args[1] + "§7' konnte nicht gefunden werden!");
+                return;
+            }
 
-			target.getStats().loadDefaults();
+            String found = this.doCommand((target) -> {
+                String toSet = args[2];
+                boolean add = args[0].equalsIgnoreCase("add"), dec = args[0].equalsIgnoreCase("decrease");
+                if (add || dec) {
+                    try {
+                        int from = Integer.parseInt(String.valueOf(type.get(target))), calc = Integer.parseInt(toSet) * (dec ? -1 : 1);
+                        toSet = String.valueOf(from + calc);
+                    } catch (NumberFormatException e) {
+                        sender.sendMessage(Main.getPrefix() + "Konnte Zahlen nicht verrechnen!");
+                        return false;
+                    }
+                }
 
-			sender.sendMessage(Main.getPrefix() + "Spieler '" + Main.getColorCode() + target.getName() + "§7' erfolgreich zurueckgesetzt!");
-		} else if (args[0].equalsIgnoreCase("odvreset")) {
-			if (target == null) {
-				for (VaroPlayer all : VaroPlayer.getVaroPlayer()) {
-					all.getStats().loadStartDefaults();
-					all.getStats().setState(PlayerState.ALIVE);
-					if (all.getTeam() != null)
-						all.getTeam().removeMember(all);
-				}
+                if (!args[0].equalsIgnoreCase("remove"))
+                    type.execute(toSet, target, sender);
+                else type.remove(target);
+                return true;
+            }, args, min - 1);
 
-				sender.sendMessage(Main.getPrefix() + "Alle Spieler erfolgreich ODV-zurueckgesetzt!");
-				return;
-			}
+            if (found == null)
+                sender.sendMessage(Main.getPrefix() + "Keine Spieler gefunden!");
+            else
+                sender.sendMessage(Main.getPrefix() + "Stat erfolgreich für " + Main.getColorCode() + found + " §7geupdated!");
+        } else if (args[0].equalsIgnoreCase("reset")) {
+            String found = this.doCommand((target) -> {
+                target.getStats().loadDefaults();
+                return true;
+            }, args, 1);
 
-			target.getStats().loadStartDefaults();
-			target.getStats().setState(PlayerState.ALIVE);
-			if (target.getTeam() != null)
-				target.getTeam().removeMember(target);
+            if (found == null)
+                sender.sendMessage(Main.getPrefix() + "Keine Spieler gefunden!");
+            else
+                sender.sendMessage(Main.getPrefix() + "Spieler " + Main.getColorCode() + found + "§7 erfolgreich zurueckgesetzt!");
+        } else if (args[0].equalsIgnoreCase("odvreset")) {
+            String found = this.doCommand((target) -> {
+                target.getStats().loadStartDefaults();
+                target.getStats().setState(PlayerState.ALIVE);
+                if (target.getTeam() != null)
+                    target.getTeam().removeMember(target);
+                return true;
+            }, args, 1);
 
-			sender.sendMessage(Main.getPrefix() + "Spieler '" + Main.getColorCode() + target.getName() + "§7' erfolgreich ODV-zurueckgesetzt!");
-		} else
-			sender.sendMessage(Main.getPrefix() + "Not found! Type " + Main.getColorCode() + label + " stats §7for help.");
-	}
+            sender.sendMessage(Main.getPrefix() + "Spieler " + Main.getColorCode() + found + "§7 erfolgreich ODV-zurueckgesetzt!");
+        } else
+            sender.sendMessage(Main.getPrefix() + "Not found! Type " + Main.getColorCode() + label + " stats §7for help.");
+    }
 }
