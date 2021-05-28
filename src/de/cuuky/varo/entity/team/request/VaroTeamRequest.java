0b6e892f9a8469276d12f0ac8432f7445bb0b3e4
UeberallGebannt <50475262+UeberallGebannt@users.noreply.github.com>
2021-05-28 19:29:19 +0200

    Fixed regex in team request

--- src/de/cuuky/varo/entity/team/request/VaroTeamRequest.java
+++ src/de/cuuky/varo/entity/team/request/VaroTeamRequest.java
@@ -56,7 +56,7 @@ public class VaroTeamRequest {
 			@Override
 			public boolean onChat(AsyncPlayerChatEvent event) {
 				String message = event.getMessage();
-				if (!message.matches("[a-z0-9]*")) {
+				if (!message.matches("[a-zA-Z0-9]*")) {
 					invitor.sendMessage(ConfigMessages.TEAMREQUEST_NO_HASHTAG, invitor);
 					return false;
 				}
