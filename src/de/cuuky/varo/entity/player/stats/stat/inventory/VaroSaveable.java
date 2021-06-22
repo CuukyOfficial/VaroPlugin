b8fb5bf0035879d881bf1350c6577bfb339ec13c
CuukyOfficial <just.cookie.jc@gmail.com>
2021-06-22 12:27:17 +0200

    Fixed VaroSaveable throwing an error when ported to another server

--- src/de/cuuky/varo/entity/player/stats/stat/inventory/VaroSaveable.java
+++ src/de/cuuky/varo/entity/player/stats/stat/inventory/VaroSaveable.java
@@ -2,7 +2,6 @@ package de.cuuky.varo.entity.player.stats.stat.inventory;
 
 import java.util.ArrayList;
 
-import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.block.Block;
 
@@ -16,165 +15,172 @@ import org.bukkit.scheduler.BukkitRunnable;
 
 public class VaroSaveable implements VaroSerializeable {
 
-	public static enum SaveableType implements VaroSerializeable {
-		@VaroSerializeField(enumValue = "CHEST")
-		CHEST(Materials.CHEST),
+    public static enum SaveableType implements VaroSerializeable {
+        @VaroSerializeField(enumValue = "CHEST")
+        CHEST(Materials.CHEST),
 
-		@VaroSerializeField(enumValue = "FURNACE")
-		FURNANCE(Materials.FURNACE);
+        @VaroSerializeField(enumValue = "FURNACE")
+        FURNANCE(Materials.FURNACE);
 
-		private Materials material;
+        private Materials material;
 
-		private SaveableType(Materials material) {
-			this.material = material;
-		}
+        private SaveableType(Materials material) {
+            this.material = material;
+        }
 
-		@Override
-		public void onDeserializeEnd() {}
+        @Override
+        public void onDeserializeEnd() {
+        }
 
-		@Override
-		public void onSerializeStart() {}
+        @Override
+        public void onSerializeStart() {
+        }
 
-		public Materials getMaterial() {
-			return material;
-		}
-	}
+        public Materials getMaterial() {
+            return material;
+        }
+    }
 
-	private static ArrayList<VaroSaveable> saveables;
+    private static ArrayList<VaroSaveable> saveables;
 
-	static {
-		saveables = new ArrayList<>();
-	}
+    static {
+        saveables = new ArrayList<>();
+    }
 
-	@VaroSerializeField(path = "id")
-	private int id;
+    @VaroSerializeField(path = "id")
+    private int id;
 
-	@VaroSerializeField(path = "playerId")
-	private int playerId;
+    @VaroSerializeField(path = "playerId")
+    private int playerId;
 
-	@VaroSerializeField(path = "type")
-	private SaveableType type;
+    @VaroSerializeField(path = "type")
+    private SaveableType type;
 
-	@VaroSerializeField(path = "blockLocation")
-	private Location blockLocation;
+    @VaroSerializeField(path = "blockLocation")
+    private Location blockLocation;
 
-	private VaroPlayer player;
-	private Block block;
+    private VaroPlayer player;
+    private Block block;
 
-	public VaroSaveable() {
-		saveables.add(this);
-	}
+    public VaroSaveable() {
+        saveables.add(this);
+    }
 
-	public VaroSaveable(SaveableType type, Location location, VaroPlayer player) {
-		this.type = type;
-		this.block = location.getBlock();
-		this.id = generateId();
-		this.player = player;
-		player.getStats().addSaveable(this);
+    public VaroSaveable(SaveableType type, Location location, VaroPlayer player) {
+        this.type = type;
+        this.block = location.getBlock();
+        this.id = generateId();
+        this.player = player;
+        player.getStats().addSaveable(this);
 
-		saveables.add(this);
-	}
+        saveables.add(this);
+    }
 
-	private int generateId() {
-		int id = JavaUtils.randomInt(1000, 9999999);
-		while (getSaveable(id) != null)
-			generateId();
+    private int generateId() {
+        int id = JavaUtils.randomInt(1000, 9999999);
+        while (getSaveable(id) != null)
+            generateId();
 
-		return id;
-	}
+        return id;
+    }
 
-	private boolean exists() {
-		return this.blockLocation.getBlock().getType() == this.type.getMaterial().parseMaterial();
-	}
+    private boolean exists() {
+        return this.blockLocation.getBlock().getType() == this.type.getMaterial().parseMaterial();
+    }
 
-	public boolean canModify(VaroPlayer player) {
-		if (this.player.getTeam() == null && !player.equals(this.player))
-			return false;
+    public boolean canModify(VaroPlayer player) {
+        if (this.player.getTeam() == null && !player.equals(this.player))
+            return false;
 
-		if (this.player.getTeam() != null && !this.player.getTeam().isMember(player))
-			return false;
+        if (this.player.getTeam() != null && !this.player.getTeam().isMember(player))
+            return false;
 
-		return true;
-	}
+        return true;
+    }
 
-	public boolean holderDead() {
-		if (this.player.getTeam() == null && this.player.getStats().isAlive())
-			return false;
+    public boolean holderDead() {
+        if (this.player.getTeam() == null && this.player.getStats().isAlive())
+            return false;
 
-		if (this.player.getTeam() != null && !this.player.getTeam().isDead())
-			return false;
+        if (this.player.getTeam() != null && !this.player.getTeam().isDead())
+            return false;
 
-		return true;
-	}
+        return true;
+    }
 
-	@Override
-	public void onDeserializeEnd() {
-		new BukkitRunnable() {
-			@Override
-			public void run() {
-				player = VaroPlayer.getPlayer(playerId);
-				block = blockLocation.getBlock();
+    @Override
+    public void onDeserializeEnd() {
+        new BukkitRunnable() {
+            @Override
+            public void run() {
+                if (player == null || blockLocation == null) {
+                    remove();
+                    return;
+                }
 
-				if (!VaroSaveable.this.exists()) {
-					remove();
-				}
-			}
-		}.runTaskLater(Main.getInstance(), 1L);
-	}
+                player = VaroPlayer.getPlayer(playerId);
+                block = blockLocation.getBlock();
 
-	@Override
-	public void onSerializeStart() {
-		this.playerId = player.getId();
-		this.blockLocation = block.getLocation();
-	}
+                if (!VaroSaveable.this.exists())
+                    remove();
+            }
+        }.runTaskLater(Main.getInstance(), 1L);
+    }
 
-	public void remove() {
-		player.getStats().removeSaveable(this);
-		saveables.remove(this);
-	}
+    @Override
+    public void onSerializeStart() {
+        this.playerId = player.getId();
+        this.blockLocation = block.getLocation();
+    }
 
-	public static VaroSaveable getByLocation(Location loc) {
-		for (VaroSaveable save : VaroSaveable.getSaveables()) {
-			Location loc1 = save.getBlock().getLocation();
-			if (loc1.getBlockX() == loc.getBlockX() && loc1.getBlockY() == loc.getBlockY() && loc.getBlockZ() == loc1.getBlockZ() && loc1.getWorld().equals(loc.getWorld()))
-				return save;
-		}
+    public void remove() {
+        if (player != null)
+            player.getStats().removeSaveable(this);
+        saveables.remove(this);
+    }
 
-		return null;
-	}
+    public static VaroSaveable getByLocation(Location loc) {
+        for (VaroSaveable save : VaroSaveable.getSaveables()) {
+            Location loc1 = save.getBlock().getLocation();
+            if (loc1.getBlockX() == loc.getBlockX() && loc1.getBlockY() == loc.getBlockY() && loc.getBlockZ() == loc1.getBlockZ() && loc1.getWorld().equals(loc.getWorld()))
+                return save;
+        }
 
-	public static VaroSaveable getSaveable(int id) {
-		for (VaroSaveable save : saveables) {
-			if (save.getId() != id)
-				continue;
+        return null;
+    }
 
-			return save;
-		}
+    public static VaroSaveable getSaveable(int id) {
+        for (VaroSaveable save : saveables) {
+            if (save.getId() != id)
+                continue;
 
-		return null;
-	}
+            return save;
+        }
 
-	public Block getBlock() {
-		return block;
-	}
+        return null;
+    }
 
-	public int getId() {
-		return id;
-	}
+    public Block getBlock() {
+        return block;
+    }
 
-	public VaroPlayer getPlayer() {
-		return player;
-	}
+    public int getId() {
+        return id;
+    }
 
-	public SaveableType getType() {
-		return type;
-	}
+    public VaroPlayer getPlayer() {
+        return player;
+    }
 
-	public static ArrayList<VaroSaveable> getSaveable(VaroPlayer player) {
-		return player.getStats().getSaveables();
-	}
+    public SaveableType getType() {
+        return type;
+    }
 
-	public static ArrayList<VaroSaveable> getSaveables() {
-		return saveables;
-	}
+    public static ArrayList<VaroSaveable> getSaveable(VaroPlayer player) {
+        return player.getStats().getSaveables();
+    }
+
+    public static ArrayList<VaroSaveable> getSaveables() {
+        return saveables;
+    }
 }
