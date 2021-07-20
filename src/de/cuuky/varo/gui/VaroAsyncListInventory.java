a06a040ffa70422b4f10e900c5a6363ec74e62d6
CuukyOfficial <just.cookie.jc@gmail.com>
2021-07-20 11:18:33 +0200

    Removed list logging

--- src/de/cuuky/varo/gui/VaroAsyncListInventory.java
+++ src/de/cuuky/varo/gui/VaroAsyncListInventory.java
@@ -4,7 +4,6 @@ import de.cuuky.cfw.hooking.HookManager;
 import de.cuuky.cfw.inventory.AdvancedInventoryManager;
 import de.cuuky.cfw.inventory.list.AdvancedAsyncListInventory;
 import de.cuuky.varo.Main;
-import de.cuuky.varo.logger.logger.EventLogger;
 import org.bukkit.entity.Player;
 
 import java.util.List;
@@ -24,13 +23,5 @@ public abstract class VaroAsyncListInventory<T> extends AdvancedAsyncListInvento
     @Override
     protected HookManager getHookManager() {
         return Main.getCuukyFrameWork().getHookManager();
-    }
-
-    @Override
-    protected String getEmptyName() {
-        if (this.getEmptyClicked() == 200 && getPlayer().hasPermission("varo.setup"))
-            Main.getDataManager().getVaroLoggerManager().getEventLogger().println(EventLogger.LogType.LOG, "An admin clicked too often!%noDiscord%");
-
-        return super.getEmptyName();
     }
 }
--- src/de/cuuky/varo/gui/VaroListInventory.java
+++ src/de/cuuky/varo/gui/VaroListInventory.java
@@ -4,7 +4,6 @@ import de.cuuky.cfw.hooking.HookManager;
 import de.cuuky.cfw.inventory.AdvancedInventoryManager;
 import de.cuuky.cfw.inventory.list.AdvancedListInventory;
 import de.cuuky.varo.Main;
-import de.cuuky.varo.logger.logger.EventLogger;
 import org.bukkit.entity.Player;
 
 import java.util.List;
@@ -24,13 +23,5 @@ public abstract class VaroListInventory<T> extends AdvancedListInventory<T> {
     @Override
     protected HookManager getHookManager() {
         return Main.getCuukyFrameWork().getHookManager();
-    }
-
-    @Override
-    protected String getEmptyName() {
-        if (this.getEmptyClicked() == 200 && getPlayer().hasPermission("varo.setup"))
-            Main.getDataManager().getVaroLoggerManager().getEventLogger().println(EventLogger.LogType.LOG, "An admin clicked too often!%noDiscord%");
-
-        return super.getEmptyName();
     }
 }
