0270f482e0f8ef0e7379eed468a043d3387a67f6
DerNoobYT <71840441+DerNoobYT@users.noreply.github.com>
2021-08-04 21:06:16 +0200

    Added ""/varo checkCombat" Command to check if you are in combat

---
+++ src/de/cuuky/varo/command/varo/CheckCombatCommand.java
@@ -0,0 +1,36 @@
+package de.cuuky.varo.command.varo;
+
+import de.cuuky.varo.Main;
+import de.cuuky.varo.combatlog.CombatlogCheck;
+import de.cuuky.varo.command.VaroCommand;
+import de.cuuky.varo.configuration.configurations.language.languages.ConfigMessages;
+import de.cuuky.varo.entity.player.VaroPlayer;
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+public class CheckCombatCommand extends VaroCommand {
+
+    public CheckCombatCommand() {
+        super("checkcombat", "Überprüft ob du dich im Combat befindest", "varo.checkcombat", "combat", "combatlog", "cl", "cc", "cls", "combatlogstatus");
+    }
+
+    @Override
+    public void onCommand(CommandSender sender, VaroPlayer vp, Command cmd, String label, String[] args) {
+        if (vp == null) {
+            sender.sendMessage(Main.getPrefix() + "Du musst ein Spieler sein!");
+            return;
+        }
+
+        if (args.length == 0) {
+            sender.sendMessage(Main.getPrefix() + ConfigMessages.VARO_COMMANDS_CHECKCOMBAT_HELP.getValue(vp));
+            return;
+        }
+
+        if (new CombatlogCheck(vp.getPlayer()).isCombatLog()) {
+            sender.sendMessage(Main.getPrefix() + ConfigMessages.VARO_COMMANDS_CHECKCOMBAT_INCOMBAT.getValue(vp));
+        } else {
+            sender.sendMessage(Main.getPrefix() + ConfigMessages.VARO_COMMANDS_CHECKCOMBAT_NOTINCOMBAT.getValue(vp));
+        }
+    }
+
+}
