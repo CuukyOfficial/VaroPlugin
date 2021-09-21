3242209409ac338f21d0ca160540dcee6aff7f53
CuukyOfficial <just.cookie.jc@gmail.com>
2021-09-21 11:00:48 +0200

    Improved vanish code

--- src/de/cuuky/varo/vanish/Vanish.java
+++ src/de/cuuky/varo/vanish/Vanish.java
@@ -10,6 +10,8 @@ import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 
 import java.util.ArrayList;
+import java.util.List;
+import java.util.function.Consumer;
 
 public class Vanish {
 
@@ -17,72 +19,61 @@ public class Vanish {
 
 		@EventHandler
 		public void onPlayerJoin(PlayerJoinEvent event) {
-			for (Vanish vanish : vanishes)
-				vanish.hideFor(event.getPlayer());
+			vanishes.forEach(va -> va.hideFor(event.getPlayer()));
 		}
 
 		@EventHandler
 		public void onPlayerLeave(PlayerQuitEvent event) {
 			Vanish v = Vanish.getVanish(event.getPlayer());
-			if (v != null)
-				v.remove();
-
-			for (Vanish vanish : vanishes)
-				vanish.unHideFor(event.getPlayer());
+			if (v != null) v.remove();
+			vanishes.forEach(va -> va.unHideFor(event.getPlayer()));
 		}
 	}
 
-	private static final ArrayList<Vanish> vanishes = new ArrayList<>();
+	private static final List<Vanish> vanishes = new ArrayList<>();
 
 	static {
 		Bukkit.getPluginManager().registerEvents(new VanishListener(), Main.getInstance());
 	}
 
-	private Player player;
+	private final Player player;
 
 	public Vanish(Player player) {
 		this.player = player;
-
-		hide();
+		this.hide();
 		vanishes.add(this);
 	}
 
+	private void changeVisibility(Consumer<Player> func) {
+		VersionUtils.getVersionAdapter().getOnlinePlayers().forEach(func);
+	}
+
 	private void hide() {
-		for (Player allplayer : VersionUtils.getVersionAdapter().getOnlinePlayers())
-			allplayer.hidePlayer(player);
+		this.changeVisibility(this::hideFor);
 	}
 
 	private void unhide() {
-		for (Player allplayer : VersionUtils.getVersionAdapter().getOnlinePlayers())
-			allplayer.showPlayer(player);
+		this.changeVisibility(this::unHideFor);
 	}
 
 	public void hideFor(Player player) {
 		player.hidePlayer(this.player);
 	}
 
-	public void remove() {
-		unhide();
-		vanishes.remove(this);
-	}
-
 	public void unHideFor(Player player) {
 		player.showPlayer(this.player);
 	}
 
+	public void remove() {
+		this.unhide();
+		vanishes.remove(this);
+	}
+
 	public Player getPlayer() {
-		return player;
+		return this.player;
 	}
 
 	public static Vanish getVanish(Player player) {
-		for (Vanish vanish : vanishes)
-			if (vanish.getPlayer().equals(player))
-				return vanish;
-
-		return null;
-	}
-
-	public static ArrayList<Vanish> getVanishes() {
-		return vanishes;
+		return vanishes.stream().filter(v -> v.getPlayer().equals(player)).findFirst().orElse(null);
 	}
 }
