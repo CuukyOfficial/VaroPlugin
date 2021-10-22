ac62a0ac875eb92226aa0829dbd797be4c5e4933
DerNoobYT <71840441+DerNoobYT@users.noreply.github.com>
2021-10-22 23:54:22 +0200

    Fixed actionbar not showing up when playTime is set to -1 but showDistanceToBorder is still enabled
    (I think that this fixes it, I did not test it!)

--- src/de/cuuky/varo/game/threads/VaroMainHeartbeatThread.java
+++ src/de/cuuky/varo/game/threads/VaroMainHeartbeatThread.java
@@ -65,11 +65,11 @@ public class VaroMainHeartbeatThread extends BukkitRunnable {
 
 			}
 
-			for (VaroPlayer vp : (ArrayList<VaroPlayer>) VaroPlayer.getOnlinePlayer().clone()) {
-				if (vp.getStats().isSpectator() || vp.isAdminIgnore())
-					continue;
+			if (ConfigSetting.PLAY_TIME.isIntActivated()) {
+				for (VaroPlayer vp : (ArrayList<VaroPlayer>) VaroPlayer.getOnlinePlayer().clone()) {
+					if (vp.getStats().isSpectator() || vp.isAdminIgnore())
+						continue;
 
-				if (ConfigSetting.PLAY_TIME.isIntActivated()) {
 					int countdown = vp.getStats().getCountdown() - 1;
 					Player p = vp.getPlayer();
 					ArrayList<String> actionbar = new ArrayList<>();
@@ -107,9 +107,16 @@ public class VaroMainHeartbeatThread extends BukkitRunnable {
 					}
 
 					vp.getStats().setCountdown(countdown);
-				} else if (showDistanceToBorder) {
+				}
+			} else if (showDistanceToBorder) {
+				for (VaroPlayer vp : (ArrayList<VaroPlayer>) VaroPlayer.getOnlinePlayer().clone()) {
+					if (vp.getStats().isSpectator() || vp.isAdminIgnore())
+						continue;
+
+					Player p = vp.getPlayer();
 					ArrayList<String> actionbar = new ArrayList<>();
-					int distance = VersionUtils.getVersion() == BukkitVersion.ONE_7 ? -1 : (int) Main.getVaroGame().getVaroWorldHandler().getVaroWorld(vp.getPlayer().getWorld()).getVaroBorder().getBorderDistanceTo(vp.getPlayer());
+
+					int distance = VersionUtils.getVersion() == BukkitVersion.ONE_7 ? -1 : (int) Main.getVaroGame().getVaroWorldHandler().getVaroWorld(p.getWorld()).getVaroBorder().getBorderDistanceTo(p);
 					if (distance != -1 && (!ConfigSetting.DISTANCE_TO_BORDER_REQUIRED.isIntActivated() || distance <= ConfigSetting.DISTANCE_TO_BORDER_REQUIRED.getValueAsInt()))
 						actionbar.add("§7Distanz zur Border: " + Main.getColorCode() + distance);
 
