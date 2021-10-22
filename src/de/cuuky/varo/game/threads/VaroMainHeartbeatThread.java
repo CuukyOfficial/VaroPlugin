d6fb9c71ab0078a7137a22cd258451c1c11d2584
DerNoobYT <71840441+DerNoobYT@users.noreply.github.com>
2021-10-23 00:00:01 +0200

    Fixed actionbar not showing up when playTime is set to -1 but showDistanceToBorder is still enabled
    (I think that this fixes it, I did not test it!)

--- src/de/cuuky/varo/game/threads/VaroMainHeartbeatThread.java
+++ src/de/cuuky/varo/game/threads/VaroMainHeartbeatThread.java
@@ -113,7 +113,7 @@ public class VaroMainHeartbeatThread extends BukkitRunnable {
 					if (vp.getStats().isSpectator() || vp.isAdminIgnore())
 						continue;
 
-					Player p = vp.getPlayer();
+					Player pp = vp.getPlayer();
 					ArrayList<String> actionbar = new ArrayList<>();
 
 					int distance = VersionUtils.getVersion() == BukkitVersion.ONE_7 ? -1 : (int) Main.getVaroGame().getVaroWorldHandler().getVaroWorld(p.getWorld()).getVaroBorder().getBorderDistanceTo(p);
