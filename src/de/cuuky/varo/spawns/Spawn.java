fd9f3d9895ea72bb8b9d3cb19c856146dfbaf483
CuukyOfficial <just.cookie.jc@gmail.com>
2021-11-23 11:26:53 +0100

    Improved performance of and fixed spawn nametags

--- src/de/cuuky/varo/spawns/Spawn.java
+++ src/de/cuuky/varo/spawns/Spawn.java
@@ -1,12 +1,5 @@
 package de.cuuky.varo.spawns;
 
-import java.util.ArrayList;
-
-import org.bukkit.Location;
-import org.bukkit.Material;
-import org.bukkit.entity.Entity;
-import org.bukkit.entity.EntityType;
-
 import de.cuuky.cfw.version.BukkitVersion;
 import de.cuuky.cfw.version.VersionUtils;
 import de.cuuky.cfw.version.types.Materials;
@@ -15,205 +8,212 @@ import de.cuuky.varo.configuration.configurations.language.languages.ConfigMessa
 import de.cuuky.varo.entity.player.VaroPlayer;
 import de.cuuky.varo.serialize.identifier.VaroSerializeField;
 import de.cuuky.varo.serialize.identifier.VaroSerializeable;
+import org.bukkit.Location;
+import org.bukkit.Material;
+import org.bukkit.entity.Entity;
+import org.bukkit.entity.EntityType;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class Spawn implements VaroSerializeable {
 
-	private static ArrayList<Spawn> spawns;
+    private static List<Spawn> spawns = new ArrayList<>();
 
-	static {
-		spawns = new ArrayList<>();
-	}
+    @VaroSerializeField(path = "location")
+    private Location location;
 
-	@VaroSerializeField(path = "location")
-	private Location location;
+    @VaroSerializeField(path = "nameTagLocation")
+    private Location nameTagLocation;
 
-	@VaroSerializeField(path = "nameTagLocation")
-	private Location nameTagLocation;
+    @VaroSerializeField(path = "nameTagName")
+    private String nameTagName;
 
-	@VaroSerializeField(path = "nameTagName")
-	private String nameTagName;
+    @VaroSerializeField(path = "number")
+    private int number;
 
-	@VaroSerializeField(path = "number")
-	private int number;
+    @VaroSerializeField(path = "playerId")
+    private int playerId;
 
-	@VaroSerializeField(path = "playerId")
-	private int playerId;
+    private Entity armorStand;
+    private VaroPlayer player;
 
-	private Entity armorStand;
-	private VaroPlayer player;
+    public Spawn() {
+        spawns.add(this);
+    }
 
-	public Spawn() {
-		spawns.add(this);
-	}
+    private Spawn(VaroPlayer player, int number, Location location) {
+        if (number == -1) {
+            this.number = generateId();
+        } else {
+            Spawn spawn = getSpawn(number);
+            if (spawn != null)
+                spawn.remove();
+        }
 
-	public Spawn(Location location) {
-		this.number = generateId();
-		this.location = location;
+        this.location = location;
+        this.player = player;
+        this.setNameTag();
+        spawns.add(this);
+    }
 
-		setNameTag();
+    public Spawn(VaroPlayer player, Location location) {
+        this(player, -1, location);
+    }
 
-		spawns.add(this);
-	}
+    public Spawn(Location location) {
+        this(null, location);
+    }
 
-	public Spawn(VaroPlayer player, Location location) {
-		this.number = generateId();
-		this.location = location;
-		this.player = player;
+    public Spawn(int number, Location location) {
+        this(null, number, location);
+    }
 
-		setNameTag();
+    private int generateId() {
+        int i = spawns.size() + 1;
+        while (getSpawn(i) != null)
+            i++;
 
-		spawns.add(this);
-	}
+        return i;
+    }
 
-	public Spawn(int number, Location location) {
-		if (getSpawn(number) != null)
-			getSpawn(number).remove();
+    private void findNameTag() {
+        if (this.nameTagLocation == null || this.nameTagName == null)
+            return;
 
-		this.number = number;
-		this.location = location;
+        for (Entity ent : nameTagLocation.getWorld().getNearbyEntities(nameTagLocation, 1d, 1d, 1d)) {
+            if (!ent.getType().toString().contains("ARMOR_STAND"))
+                continue;
 
-		setNameTag();
+            try {
+                String entName = ((String) ent.getClass().getMethod("getCustomName").invoke(ent));
+                if (!entName.equals(this.nameTagName))
+                    continue;
 
-		spawns.add(this);
-	}
+                this.armorStand = ent;
+                String newName = this.getNametagName();
+                if (!ConfigSetting.SET_NAMETAGS_OVER_SPAWN.getValueAsBoolean()) {
+                    this.removeNameTag();
+                } else if (!newName.equals(this.nameTagName)) {
+                    this.updateNametag(this.getNametagName());
+                }
+                return;
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        }
+    }
 
-	private int generateId() {
-		int i = spawns.size() + 1;
-		while (getSpawn(i) != null)
-			i++;
+    private String getNametagName() {
+        return this.player == null ?
+            ConfigMessages.SPAWNS_SPAWN_NUMBER.getValue().replace("%number%", String.valueOf(number)) :
+            ConfigMessages.SPAWNS_SPAWN_PLAYER.getValue(null, this.player).replace("%number%", String.valueOf(number));
+    }
 
-		return i;
-	}
+    private void remove() {
+        this.removeNameTag();
+        spawns.remove(this);
+    }
 
-	private String getNametagName() {
-		return this.player == null ? ConfigMessages.SPAWNS_SPAWN_NUMBER.getValue().replace("%number%", String.valueOf(number)) : ConfigMessages.SPAWNS_SPAWN_PLAYER.getValue(null, this.player).replace("%number%", String.valueOf(number));
-	}
+    private void removeNameTag() {
+        if (this.armorStand == null)
+            return;
 
-	private void remove() {
-		removeNameTag();
-		spawns.remove(this);
-	}
+        this.armorStand.remove();
+        this.armorStand = null;
+    }
 
-	private void removeNameTag() {
-		if (armorStand != null)
-			armorStand.remove();
-	}
+    private void setNameTag() {
+        if (!ConfigSetting.SET_NAMETAGS_OVER_SPAWN.getValueAsBoolean() ||
+            !VersionUtils.getVersion().isHigherThan(BukkitVersion.ONE_7))
+            return;
 
-	private void setNameTag() {
-		if (!ConfigSetting.SET_NAMETAGS_OVER_SPAWN.getValueAsBoolean() || !VersionUtils.getVersion().isHigherThan(BukkitVersion.ONE_7))
-			return;
+        nameTagLocation = location.clone().add(0, ConfigSetting.NAMETAG_SPAWN_HEIGHT.getValueAsInt(), 0);
+        armorStand = location.getWorld().spawnEntity(nameTagLocation, EntityType.ARMOR_STAND);
+        this.updateNametag(this.getNametagName());
+    }
 
-		nameTagLocation = location.clone().add(0, ConfigSetting.NAMETAG_SPAWN_HEIGHT.getValueAsInt(), 0);
-		armorStand = location.getWorld().spawnEntity(nameTagLocation, EntityType.ARMOR_STAND);
+    public void updateNametag(String name) {
+        if (this.armorStand == null)
+            return;
 
-		updateNametag();
-	}
+        VersionUtils.getVersionAdapter().setArmorstandAttributes(this.armorStand,
+            false, true, false, this.nameTagName = name);
+    }
 
-	public void updateNametag() {
-		if(this.armorStand != null)
-			VersionUtils.getVersionAdapter().setArmorstandAttributes(this.armorStand, false, true, false, this.getNametagName());
-	}
+    public void delete() {
+        location.getBlock().setType(Materials.GRASS_BLOCK.parseMaterial());
+        location.add(0, 1, 0);
+        location.clone().add(1, 0, 0).getBlock().setType(Material.AIR);
+        location.clone().add(-1, 0, 0).getBlock().setType(Material.AIR);
+        location.clone().add(0, 0, 1).getBlock().setType(Material.AIR);
+        location.clone().add(0, 0, -1).getBlock().setType(Material.AIR);
 
-	public void delete() {
-		location.getBlock().setType(Materials.GRASS_BLOCK.parseMaterial());
-		location.add(0, 1, 0);
-		location.clone().add(1, 0, 0).getBlock().setType(Material.AIR);
-		location.clone().add(-1, 0, 0).getBlock().setType(Material.AIR);
-		location.clone().add(0, 0, 1).getBlock().setType(Material.AIR);
-		location.clone().add(0, 0, -1).getBlock().setType(Material.AIR);
+        remove();
+    }
 
-		remove();
-	}
+    @Override
+    public void onDeserializeEnd() {
+        if (this.location == null)
+            remove();
 
-	@Override
-	public void onDeserializeEnd() {
-		if (this.location == null)
-			remove();
+        if (playerId != -1)
+            this.player = VaroPlayer.getPlayer(playerId);
 
-		if (playerId != -1)
-			this.player = VaroPlayer.getPlayer(playerId);
+        this.findNameTag();
+        if (this.armorStand == null)
+            this.setNameTag();
+    }
 
-		if (nameTagLocation != null && nameTagName != null) {
-			for (Entity ent : nameTagLocation.getWorld().getEntities()) {
-				if (!ent.getType().toString().contains("ARMOR_STAND"))
-					continue;
+    @Override
+    public void onSerializeStart() {
+        this.playerId = player != null ? player.getId() : -1;
+    }
 
-				try {
-					String entName = ((String) ent.getClass().getMethod("getCustomName").invoke(ent));
-					if (ent.getLocation().distance(nameTagLocation) < 1 && entName.equals(nameTagName)) {
-						this.armorStand = ent;
+    public Location getLocation() {
+        return location;
+    }
 
-						if (!ConfigSetting.SET_NAMETAGS_OVER_SPAWN.getValueAsBoolean() && armorStand != null) {
-							this.armorStand.remove();
-							this.armorStand = null;
-						} else {
-							updateNametag();
-						}
-					}
-				} catch (Exception e) {
-					e.printStackTrace();
-				}
-			}
+    public int getNumber() {
+        return number;
+    }
 
-			if (this.armorStand == null)
-				setNameTag();
-		}
-	}
+    public VaroPlayer getPlayer() {
+        return player;
+    }
 
-	@Override
-	public void onSerializeStart() {
-		this.playerId = player != null ? player.getId() : -1;
-	}
+    public void setPlayer(VaroPlayer player) {
+        this.player = player;
+        this.updateNametag(this.getNametagName());
+    }
 
-	public Location getLocation() {
-		return location;
-	}
+    public static Spawn getSpawn(int number) {
+        for (Spawn spawn : spawns) {
+            if (spawn.getNumber() != number)
+                continue;
 
-	public Location getNameTagLocation() {
-		return nameTagLocation;
-	}
+            return spawn;
+        }
 
-	public int getNumber() {
-		return number;
-	}
+        return null;
+    }
 
-	public VaroPlayer getPlayer() {
-		return player;
-	}
+    public static Spawn getSpawn(VaroPlayer player) {
+        for (Spawn spawn : spawns) {
+            if (spawn.getPlayer() == null || !spawn.getPlayer().equals(player))
+                continue;
 
-	public void setPlayer(VaroPlayer player) {
-		this.player = player;
+            return spawn;
+        }
 
-		updateNametag();
-	}
+        return null;
+    }
 
-	public static Spawn getSpawn(int number) {
-		for (Spawn spawn : spawns) {
-			if (spawn.getNumber() != number)
-				continue;
+    public static List<Spawn> getSpawnsClone() {
+        return new ArrayList<>(spawns);
+    }
 
-			return spawn;
-		}
-
-		return null;
-	}
-
-	public static Spawn getSpawn(VaroPlayer player) {
-		for (Spawn spawn : spawns) {
-			if (spawn.getPlayer() == null || !spawn.getPlayer().equals(player))
-				continue;
-
-			return spawn;
-		}
-
-		return null;
-	}
-
-	public static ArrayList<Spawn> getSpawnsClone() {
-		return new ArrayList<>(spawns);
-	}
-
-	public static ArrayList<Spawn> getSpawns() {
-		return spawns;
-	}
+    public static List<Spawn> getSpawns() {
+        return spawns;
+    }
 }
