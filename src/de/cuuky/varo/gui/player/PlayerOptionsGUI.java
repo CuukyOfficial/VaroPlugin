b4727d3166e24a3440e6603764599bfcfcfe5d33
CuukyOfficial <just.cookie.jc@gmail.com>
2021-11-23 20:50:00 +0100

    Reimplemented old player options menu

--- src/de/cuuky/varo/gui/player/PlayerOptionsGUI.java
+++ src/de/cuuky/varo/gui/player/PlayerOptionsGUI.java
@@ -2,25 +2,34 @@ package de.cuuky.varo.gui.player;
 
 import de.cuuky.cfw.hooking.hooks.chat.ChatHook;
 import de.cuuky.cfw.hooking.hooks.chat.ChatHookHandler;
+import de.cuuky.cfw.inventory.AdvancedInventory;
 import de.cuuky.cfw.inventory.ItemClick;
 import de.cuuky.cfw.inventory.confirm.ConfirmInventory;
+import de.cuuky.cfw.inventory.list.AdvancedItemShowInventory;
+import de.cuuky.cfw.inventory.page.AdvancedInfiniteInventory;
 import de.cuuky.cfw.utils.item.BuildItem;
+import de.cuuky.cfw.version.types.Materials;
 import de.cuuky.varo.Main;
 import de.cuuky.varo.entity.player.VaroPlayer;
 import de.cuuky.varo.entity.player.stats.StatType;
-import de.cuuky.varo.gui.VaroListInventory;
+import de.cuuky.varo.entity.player.stats.stat.PlayerState;
+import de.cuuky.varo.gui.VaroInventory;
+import de.cuuky.varo.utils.ArrayUtils;
 import org.bukkit.entity.Player;
 import org.bukkit.event.player.AsyncPlayerChatEvent;
 import org.bukkit.inventory.ItemStack;
 
 import java.util.Arrays;
+import java.util.function.Consumer;
+import java.util.function.Predicate;
 
-public class PlayerOptionsGUI extends VaroListInventory<StatType> {
+public class PlayerOptionsGUI extends AdvancedInfiniteInventory {
 
     private final VaroPlayer target;
+    private int index;
 
     public PlayerOptionsGUI(Player opener, VaroPlayer target) {
-        super(Main.getCuukyFrameWork().getAdvancedInventoryManager(), opener, Arrays.asList(StatType.values()));
+        super(Main.getCuukyFrameWork().getAdvancedInventoryManager(), opener);
 
         this.target = target;
     }
@@ -30,28 +39,28 @@ public class PlayerOptionsGUI extends VaroListInventory<StatType> {
         return Main.getColorCode() + target.getName() + " §7(" + target.getId() + ")";
     }
 
-//    @Override
-//    public int getSize() {
-//        return this.getRecommendedSize(9);
-//    }
+    @Override
+    public int getSize() {
+        return 45;
+    }
 
     public VaroPlayer getTarget() {
         return this.target;
     }
 
-    @Override
-    protected ItemStack getItemStack(StatType statType) {
-        return new BuildItem().itemstack(statType.getIcon()).displayName(statType.getDisplayName())
-                .lore("§7Current: " + Main.getColorCode() + statType.get(this.target),
-                        "", "Left-Click to set value", "Right-Click to reset").deleteDamageAnnotation().build();
+    private void openReset(StatType type) {
+        this.openNext(new ConfirmInventory(this, "§cReset " + type.name() + "?", (accept) -> {
+            if (accept) type.remove(this.target);
+            this.open();
+        }));
     }
 
-    @Override
-    protected ItemClick getClick(StatType statType) {
+    private ItemClick getClick(StatType statType) {
         return (event) -> {
             if (event.isLeftClick()) {
                 Main.getCuukyFrameWork().getHookManager().registerHook(new ChatHook(getPlayer(),
-                        Main.getPrefix() + "Enter new " + Main.getColorCode() + statType.name() + "§7: §8('!cancel' to cancel)", new ChatHookHandler() {
+                    Main.getPrefix() + "Enter new " + Main.getColorCode() + statType.name() +
+                        "§7: §8('!cancel' to cancel)", new ChatHookHandler() {
 
                     @Override
                     public boolean onChat(AsyncPlayerChatEvent event) {
@@ -60,17 +69,59 @@ public class PlayerOptionsGUI extends VaroListInventory<StatType> {
                             return true;
                         }
 
-                        boolean success = statType.execute(event.getMessage(), target, getPlayer());
-                        if (success) open();
+                        boolean success = statType.execute(event.getMessage(), target);
+                        if (success) {
+                            open();
+                        } else {
+                            getPlayer().sendMessage(
+                                Main.getPrefix() + "Konnte nicht gesetzt werden! Versuche es erneut:");
+                        }
                         return success;
                     }
                 }));
                 this.close();
             } else if (event.isRightClick())
-                this.openNext(new ConfirmInventory(this, "§cReset " + statType.name() + "?", (accept) -> {
-                    if (accept) statType.remove(this.target);
-                    this.open();
-                }));
+                this.openReset(statType);
         };
+    }
+
+    private void applyToTypes(Predicate<Object> objectPredicate, Consumer<StatType> forEach, int add) {
+        Arrays.stream(StatType.values()).filter(s -> objectPredicate.test(s.get(target))).forEach(s -> {
+            forEach.accept(s);
+            this.index += add;
+        });
+    }
+
+    private ItemStack getItemStack(StatType statType) {
+        return new BuildItem().itemstack(statType.getIcon()).displayName(statType.getDisplayName())
+            .lore("§7Current: " + Main.getColorCode() + statType.get(this.target),
+                "", "Left-Click to change value", "Right-Click to reset").deleteDamageAnnotation().build();
+    }
+
+    private void addLeftClickable(Predicate<Object> objectPredicate, Consumer<StatType> leftClick) {
+        this.applyToTypes(objectPredicate, s -> this.addItem(index, this.getItemStack(s), (event) -> {
+            if (event.isLeftClick()) {
+                leftClick.accept(s);
+            } else this.openReset(s);
+        }), 1);
+    }
+
+    @Override
+    public void refreshContent() {
+        this.index = 0;
+        this.applyToTypes(i -> i instanceof Integer, s -> {
+            this.addItem(index, new BuildItem().material(Materials.ROSE_RED).displayName("§c-").build(),
+                (e) -> s.execute(String.valueOf(((int) s.get(this.target)) - 1), this.target));
+            this.addItem(index + 4, this.getItemStack(s), this.getClick(s));
+            this.addItem(index + 8, new BuildItem().material(Materials.ROSE_RED).displayName("§a+").build(),
+                (e) -> s.execute(((int) s.get(this.target)) + 1, this.target));
+        }, 9);
+        this.index += 9;
+
+        this.addLeftClickable(b -> b instanceof Boolean, s -> s.execute(!((Boolean) s.get(this.target)), this.target));
+        this.applyToTypes(i -> i instanceof String, s ->
+            this.addItem(index, this.getItemStack(s), this.getClick(s)), 1);
+        this.addLeftClickable(b -> b instanceof PlayerState,
+            s -> s.execute(ArrayUtils.getNext(s.get(this.target), PlayerState.values()), this.target));
     }
 }
