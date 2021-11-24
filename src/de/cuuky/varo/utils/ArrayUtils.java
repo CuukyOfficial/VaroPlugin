8ab576fac81a63bef389d27677271993d423138c
CuukyOfficial <just.cookie.jc@gmail.com>
2021-11-24 11:14:15 +0100

    Improved ArrayUtils

--- src/de/cuuky/varo/utils/ArrayUtils.java
+++ src/de/cuuky/varo/utils/ArrayUtils.java
@@ -5,7 +5,7 @@ public class ArrayUtils {
     public static <T> T getNext(T search, T[] arr) {
         for (int i = 0; i < arr.length; i++)
             if (arr[i] == search)
-                return arr[i == arr.length - 1 ? 0 : i + 1];
+                return arr[(i + 1) % arr.length];
 
         return null;
     }
