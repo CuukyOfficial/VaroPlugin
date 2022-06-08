e87a990b8ed3711e7994c0d4382cde1adb5296a6
CuukyOfficial <just.cookie.jc@gmail.com>
2022-06-08 20:03:24 +0200

    Removed unused handlerlist

--- src/de/varoplugin/varo/api/event/game/VaroGameEvent.java
+++ src/de/varoplugin/varo/api/event/game/VaroGameEvent.java
@@ -2,11 +2,8 @@ package de.varoplugin.varo.api.event.game;
 
 import de.varoplugin.varo.api.event.VaroEvent;
 import de.varoplugin.varo.game.Varo;
-import org.bukkit.event.HandlerList;
 
 public abstract class VaroGameEvent extends VaroEvent {
-
-    private static final HandlerList handlers = new HandlerList();
 
     private final Varo varo;
 
@@ -16,13 +13,5 @@ public abstract class VaroGameEvent extends VaroEvent {
 
     public Varo getGame() {
         return this.varo;
-    }
-
-    public HandlerList getHandlers() {
-        return handlers;
-    }
-
-    public static HandlerList getHandlerList() {
-        return handlers;
     }
 }
