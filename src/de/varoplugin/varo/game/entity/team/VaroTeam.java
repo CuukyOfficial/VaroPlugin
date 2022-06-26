4d0ebf486ad6bd24750214814a4f593a9fcf31bd
CuukyOfficial <just.cookie.jc@gmail.com>
2022-06-26 19:17:03 +0200

    Added VaroTeamable

--- src/de/varoplugin/varo/game/entity/team/VaroTeam.java
+++ src/de/varoplugin/varo/game/entity/team/VaroTeam.java
@@ -2,8 +2,10 @@ package de.varoplugin.varo.game.entity.team;
 
 import de.varoplugin.varo.game.entity.VaroEntity;
 
+import java.util.Collection;
+
 public interface VaroTeam extends VaroEntity {
 
-
+    Collection<VaroTeamable> getMember();
 
 }
