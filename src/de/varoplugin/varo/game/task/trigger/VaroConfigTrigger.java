ddf4fd06afd4d8b60e7dd17d676a6b66c6c9513b
CuukyOfficial <just.cookie.jc@gmail.com>
2022-07-04 15:14:55 +0200

    Added VaroConfigTrigger

---
+++ src/de/varoplugin/varo/game/task/trigger/VaroConfigTrigger.java
@@ -0,0 +1,33 @@
+package de.varoplugin.varo.game.task.trigger;
+
+import de.varoplugin.varo.api.task.trigger.VaroTrigger;
+import de.varoplugin.varo.config.VaroConfig;
+import de.varoplugin.varo.game.Varo;
+
+public class VaroConfigTrigger extends GameTrigger {
+
+    private VaroConfig.VaroBoolConfigEntry entry;
+
+    public VaroConfigTrigger(Varo varo, VaroConfig.VaroBoolConfigEntry entry, boolean match) {
+        super(varo, match);
+        this.entry = entry;
+    }
+
+    public VaroConfigTrigger(Varo varo, VaroConfig.VaroBoolConfigEntry entry) {
+        this(varo, entry, true);
+    }
+
+    @Override
+    protected boolean isTriggered() {
+        return this.entry.getValue();
+    }
+
+    // TODO: Varo config entry change event
+
+    @Override
+    public VaroTrigger clone() {
+        VaroConfigTrigger trigger = (VaroConfigTrigger) super.clone();
+        trigger.entry = this.entry;
+        return trigger;
+    }
+}
--- src/de/varoplugin/varo/game/task/trigger/builder/VaroTriggerBuilder.java
+++ src/de/varoplugin/varo/game/task/trigger/builder/VaroTriggerBuilder.java
@@ -1,11 +1,13 @@
 package de.varoplugin.varo.game.task.trigger.builder;
 
+import de.varoplugin.varo.config.VaroConfig;
 import de.varoplugin.varo.game.Varo;
 import de.varoplugin.varo.game.VaroState;
 import de.varoplugin.varo.api.task.trigger.VaroTrigger;
 import de.varoplugin.varo.game.task.trigger.GameStateTrigger;
 import de.varoplugin.varo.api.task.trigger.builder.AbstractTriggerBuilder;
 import de.varoplugin.varo.api.task.trigger.builder.TriggerBuilder;
+import de.varoplugin.varo.game.task.trigger.VaroConfigTrigger;
 
 import java.util.function.Consumer;
 
@@ -19,31 +21,30 @@ public class VaroTriggerBuilder extends AbstractTriggerBuilder implements Trigge
     }
 
     public VaroTriggerBuilder when(VaroState state) {
-        this.when(new GameStateTrigger(this.varo, state));
-        return this;
+        return this.when(new GameStateTrigger(this.varo, state));
     }
 
     public VaroTriggerBuilder whenNot(VaroState state) {
-        this.when(new GameStateTrigger(this.varo, state, false));
-        return this;
+        return this.when(new GameStateTrigger(this.varo, state, false));
     }
 
     @Override
     public VaroTriggerBuilder when(VaroTrigger trigger) {
-        super.when(trigger);
-        return this;
+        return (VaroTriggerBuilder) super.when(trigger);
     }
 
     @Override
     public VaroTriggerBuilder and(VaroTrigger... triggers) {
-        super.and(triggers);
-        return this;
+        return (VaroTriggerBuilder) super.and(triggers);
+    }
+
+    public VaroTriggerBuilder and(VaroConfig.VaroBoolConfigEntry entry) {
+        return (VaroTriggerBuilder) super.and(new VaroConfigTrigger(this.varo, entry));
     }
 
     public VaroTriggerBuilder and(Consumer<VaroTriggerBuilder> and) {
         VaroTriggerBuilder vtb = new VaroTriggerBuilder(this.varo);
         and.accept(vtb);
-        this.and(vtb);
-        return this;
+        return (VaroTriggerBuilder) this.and(vtb);
     }
 }
