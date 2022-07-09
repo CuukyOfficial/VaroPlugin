7ccfa686be34cfbbac9fdeede3c587c861edfee3
CuukyOfficial <just.cookie.jc@gmail.com>
2022-07-09 14:39:50 +0200

    Added protectable add effect

--- src/de/varoplugin/varo/ui/listener/PlayerAddProtectablePrinter.java
+++ src/de/varoplugin/varo/ui/listener/PlayerAddProtectablePrinter.java
@@ -2,6 +2,7 @@ package de.varoplugin.varo.ui.listener;
 
 import de.varoplugin.varo.api.event.game.world.protectable.VaroProtectableAddEvent;
 import org.bukkit.Bukkit;
+import org.bukkit.Effect;
 import org.bukkit.event.EventHandler;
 
 public class PlayerAddProtectablePrinter extends UiListener {
@@ -10,5 +11,9 @@ public class PlayerAddProtectablePrinter extends UiListener {
     public void onProtectableAdd(VaroProtectableAddEvent event) {
         // Won't work for anything other than a player
         Bukkit.getPlayer(event.getProtectable().getHolder().getUuid()).sendMessage("Block saved");
+
+        for (int i = 0; i < 6; i++)
+            event.getProtectable().getBlock().getWorld().playEffect(
+                    event.getProtectable().getBlock().getLocation(), Effect.ENDER_SIGNAL, 1);
     }
 }
