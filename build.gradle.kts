a12ee4dfb807e2bde3c37ec3874dd054cb2c5ee0
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2023-07-17 08:57:17 +0200

    Bump JDA version

--- build.gradle.kts
+++ build.gradle.kts
@@ -77,7 +77,7 @@ dependencies {
 
     runtimeDownload("com.google.code.gson:gson:2.10.1")
     runtimeDownload("com.google.guava:guava:19.0")
-    runtimeDownload("net.dv8tion:JDA:5.0.0-beta.10") {
+    runtimeDownload("net.dv8tion:JDA:5.0.0-beta.12") {
         exclude(module = "opus-java")
     }
     runtimeDownload("com.github.pengrad:java-telegram-bot-api:5.4.0")
