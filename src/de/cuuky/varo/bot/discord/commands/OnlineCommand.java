13a1ae380569b7aa6ec9546cbb0db3826b1c91e9
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2023-10-06 16:01:08 +0200

    Respect Discord's character limit when replying to commands

--- src/de/cuuky/varo/bot/discord/commands/OnlineCommand.java
+++ src/de/cuuky/varo/bot/discord/commands/OnlineCommand.java
@@ -2,10 +2,11 @@ package de.cuuky.varo.bot.discord.commands;
 
 import java.awt.Color;
 
-import org.bukkit.GameMode;
-
 import de.cuuky.varo.bot.discord.DiscordBotCommand;
+import de.cuuky.varo.configuration.configurations.config.ConfigSetting;
 import de.cuuky.varo.entity.player.VaroPlayer;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 
 public class OnlineCommand extends DiscordBotCommand {
@@ -20,17 +21,16 @@ public class OnlineCommand extends DiscordBotCommand {
 
 	@Override
 	public void onExecute(SlashCommandInteraction event) {
-	    String players = "";
+	    StringBuilder players = new StringBuilder();
 		for (VaroPlayer vp : VaroPlayer.getOnlinePlayer()) {
-			if (vp.getPlayer().getGameMode() != GameMode.SURVIVAL)
-				continue;
-
-			if (players.equals(""))
-				players = vp.getName();
-			else
-				players = players + ", " + vp.getName();
+		    if (players.length() >= (ConfigSetting.DISCORDBOT_USE_EMBEDS.getValueAsBoolean() ? MessageEmbed.DESCRIPTION_MAX_LENGTH : Message.MAX_CONTENT_LENGTH) - 23) {
+		        players.append(", ...");
+		        break;
+		    } else if (players.length() == 0)
+		        players.append(vp.getName());
+		    else
+		        players.append(", ").append(vp.getName());
 		}
-
-		getDiscordBot().reply(players, "Online (" + VaroPlayer.getOnlinePlayer().size() + ")", Color.BLUE, event);
+		getDiscordBot().reply(players.toString(), "Online (" + VaroPlayer.getOnlinePlayer().size() + ")", Color.BLUE, event);
 	}
 }
--- src/de/cuuky/varo/bot/discord/commands/RegisteredCommand.java
+++ src/de/cuuky/varo/bot/discord/commands/RegisteredCommand.java
@@ -3,7 +3,10 @@ package de.cuuky.varo.bot.discord.commands;
 import java.awt.Color;
 
 import de.cuuky.varo.bot.discord.DiscordBotCommand;
+import de.cuuky.varo.configuration.configurations.config.ConfigSetting;
 import de.cuuky.varo.entity.player.VaroPlayer;
+import net.dv8tion.jda.api.entities.Message;
+import net.dv8tion.jda.api.entities.MessageEmbed;
 import net.dv8tion.jda.api.interactions.commands.SlashCommandInteraction;
 
 public class RegisteredCommand extends DiscordBotCommand {
@@ -18,14 +21,16 @@ public class RegisteredCommand extends DiscordBotCommand {
 
 	@Override
 	public void onExecute(SlashCommandInteraction event) {
-		String players = "";
-		for (VaroPlayer vp : VaroPlayer.getVaroPlayers()) {
-			if (players.equals(""))
-				players = vp.getName();
-			else
-				players = players + ", " + vp.getName();
-		}
-
-		getDiscordBot().reply(players, "Participating Players (" + VaroPlayer.getVaroPlayers().size() + ")", Color.BLUE, event);
+		StringBuilder players = new StringBuilder();
+        for (VaroPlayer vp : VaroPlayer.getVaroPlayers()) {
+            if (players.length() >= (ConfigSetting.DISCORDBOT_USE_EMBEDS.getValueAsBoolean() ? MessageEmbed.DESCRIPTION_MAX_LENGTH : Message.MAX_CONTENT_LENGTH) - 23) {
+                players.append(", ...");
+                break;
+            } else if (players.length() == 0)
+                players.append(vp.getName());
+            else
+                players.append(", ").append(vp.getName());
+        }
+		getDiscordBot().reply(players.toString(), "Participating Players (" + VaroPlayer.getVaroPlayers().size() + ")", Color.BLUE, event);
 	}
 }
