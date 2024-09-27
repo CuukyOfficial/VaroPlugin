55184a2a02935d0de9adb80dfa4e69a0253a6a94
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2024-09-27 18:41:34 +0200

    Fix players being able to enter the nether before the game starts

--- src/de/cuuky/varo/listener/PlayerTeleportListener.java
+++ src/de/cuuky/varo/listener/PlayerTeleportListener.java
@@ -5,6 +5,7 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.player.PlayerTeleportEvent;
 
+import de.cuuky.varo.Main;
 import de.cuuky.varo.configuration.configurations.config.ConfigSetting;
 import de.cuuky.varo.entity.player.VaroPlayer;
 
@@ -12,15 +13,20 @@ public class PlayerTeleportListener implements Listener {
 
 	@EventHandler
 	public void onTeleport(PlayerTeleportEvent event) {
+	    VaroPlayer vp = VaroPlayer.getPlayer(event.getPlayer());
 		if (event.getCause().equals(PlayerTeleportEvent.TeleportCause.NETHER_PORTAL)) {
 			if (event.getFrom().getWorld().getEnvironment() == World.Environment.NORMAL && event.getTo().getWorld().getEnvironment() == World.Environment.NETHER) {
+			    if (!event.getPlayer().isOp() && !Main.getVaroGame().hasStarted()) {
+			        event.setCancelled(true);
+			        return;
+			    }
+			    
 				if (event.getFrom().distance(event.getFrom().getWorld().getSpawnLocation()) < 500) {
 					event.getTo().getWorld().setSpawnLocation(event.getTo().getBlockX(), event.getTo().getBlockY(), event.getTo().getBlockZ());
 				}
 			}
 		}
 
-		VaroPlayer vp = VaroPlayer.getPlayer(event.getPlayer());
 		if (!vp.getStats().isSpectator() && !vp.isAdminIgnore() || event.getPlayer().isOp())
 			return;
 
