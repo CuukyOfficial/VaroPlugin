be1e5aeac4389a3e6fc07955f4111a72d6b710f5
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2024-10-26 16:24:44 +0200

    Move sources to maven default location

--- build.gradle.kts
+++ build.gradle.kts
@@ -22,23 +22,6 @@ java {
 tasks.compileJava { options.encoding = "UTF-8" }
 tasks.javadoc { options.encoding = "UTF-8" }
 
-sourceSets {
-    main {
-        java {
-            srcDir("src")
-        }
-        resources {
-            srcDir("resources")
-        }
-    }
-
-    test {
-        java {
-            srcDir("test")
-        }
-    }
-}
-
 repositories {
     mavenCentral()
     maven("https://s01.oss.sonatype.org/content/repositories/snapshots/")
