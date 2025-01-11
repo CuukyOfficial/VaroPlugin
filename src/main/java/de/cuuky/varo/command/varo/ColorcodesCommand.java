13bbf81072bb412adbea3db820cf8a4ea31cd20c
Kalibrier <157916660+Kalibrier@users.noreply.github.com>
2025-01-11 21:52:08 +0100

    Added Colorcodes Command (#107)
    * Update VaroCommand.java
    
    * Update CheckCombatCommand.java
    
    * Create ColorcodesCommand.java
    
    * Update ColorcodesCommand.java
    
    * Update src/main/java/de/cuuky/varo/command/varo/ColorcodesCommand.java
    
    Co-authored-by: Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
    
    * Update src/main/java/de/cuuky/varo/command/varo/ColorcodesCommand.java
    
    Co-authored-by: Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
    
    * Update src/main/java/de/cuuky/varo/command/varo/ColorcodesCommand.java
    
    Co-authored-by: Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
    
    * Update CheckCombatCommand.java
    
    * Update ColorcodesCommand.java
    
    ---------
    
    Co-authored-by: Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>

---
+++ src/main/java/de/cuuky/varo/command/varo/ColorcodesCommand.java
@@ -0,0 +1,35 @@
+package de.cuuky.varo.command.varo;
+
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandSender;
+
+import de.cuuky.varo.Main;
+import de.cuuky.varo.command.VaroCommand;
+import de.cuuky.varo.player.VaroPlayer;
+
+public class ColorcodesCommand extends VaroCommand {
+
+	public ColorcodesCommand() {
+		super("colorcodes", "Zeigt alle Colorcodes an", "varo.colorcodes", "formatting");
+	}
+
+	@Override
+	public void onCommand(CommandSender sender, VaroPlayer vp, Command cmd, String label, String[] args) {
+
+            sender.sendMessage("§7==============================");
+            sender.sendMessage("&0 = §0Black                §f&1 = §1Dark Blue");
+            sender.sendMessage("&2 = §2Dark Green      §f&3 = §3Dark Aqua");
+            sender.sendMessage("&4 = §4Dark Red          §f&5 = §5Dark Purple");
+            sender.sendMessage("&6 = §6Gold                  §f&7 = §7Gray");
+            sender.sendMessage("&8 = §8Dark Gray        §f&9 = §9Blue");
+            sender.sendMessage("&a = §aGreen               §f&b = §bAqua");
+            sender.sendMessage("&c = §cRed                   §f&d = §dLight Purple");
+            sender.sendMessage("&e = §eYellow               §f&f = §fWhite");
+            sender.sendMessage("§7==============================");
+            sender.sendMessage("&k = §kMagic§r§f                 &l = §lBold");
+            sender.sendMessage("&m = §mStrikethrough§r§f  &n = §nUnderline");
+            sender.sendMessage("&o = §oItalic§r§f                 &r = §rReset");
+            sender.sendMessage("§7==============================");
+        
+	}
+}
