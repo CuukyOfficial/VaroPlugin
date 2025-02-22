4cac771370fbf8de4e4fbf578a1dcd69c7ffbe45
Almighty-Satan <43804777+Almighty-Satan@users.noreply.github.com>
2025-02-22 09:03:04 +0100

    Implement unimplemented placeholders

--- src/main/java/de/cuuky/varo/config/language/Placeholders.java
+++ src/main/java/de/cuuky/varo/config/language/Placeholders.java
@@ -18,6 +18,8 @@
 
 package de.cuuky.varo.config.language;
 
+import java.time.LocalDate;
+import java.time.LocalDateTime;
 import java.util.Collections;
 import java.util.function.Function;
 
@@ -51,23 +53,25 @@ public final class Placeholders {
                 .constant("prefix", Main.getPrefix())
                 .constant("colorcode", Main.getColorCode())
                 .constant("discord", ConfigSetting.DISCORDBOT_INVITELINK.getValueAsString())
-                .variable("players", () -> "TODO")
-                .variable("alive", () -> "TODO")
-                .variable("online", () -> "TODO")
-                .variable("project-hour", () -> String.format("%02d", Main.getVaroGame().getProjectTime() / 3600))
-                .variable("project-minute", () -> String.format("%02d", (Main.getVaroGame().getProjectTime() / 60) % 60))
-                .variable("project-second", () -> String.format("%02d", Main.getVaroGame().getProjectTime() % 60))
+                .variable("num-players", () -> String.valueOf(VaroPlayer.getVaroPlayers().size()))
+                .variable("num-alive", () -> String.valueOf(VaroPlayer.getAlivePlayer().size()))
+                .variable("num-online", () -> String.valueOf(VaroPlayer.getOnlineAndAlivePlayer().size()))
+                .variable("project-hour", () -> toPaddedString(Main.getVaroGame().getProjectTime() / 3600))
+                .variable("project-minute", () -> toPaddedString((Main.getVaroGame().getProjectTime() / 60) % 60))
+                .variable("project-second", () -> toPaddedString(Main.getVaroGame().getProjectTime() % 60))
                 .variable("border-size", () -> String.valueOf(Main.getVaroGame().getVaroWorldHandler().getBorderSize(null)))
                 .variable("spawn-world", () -> Main.getVaroGame().getVaroWorldHandler().getMainWorld().getWorld().getName())
-//                .withArgs("top-player", args -> {
-//                    if (args.size() != 2) return "INVALID_ARGS";
-//                    try {
-//                        VaroPlayer player = Main.getVaroGame().getTopScores().getPlayer(Integer.parseInt(args.get(0)));
-//                        return player.;
-//                    } catch (NumberFormatException e) {
-//                        return "INVALID_ARGS";
-//                    }
-//                })
+                .withArgs("top-player", args -> {
+                    if (args.size() == 0 || args.size() > 2) return "INVALID_ARGS";
+                    try {
+                        VaroPlayer player = Main.getVaroGame().getTopScores().getPlayer(Integer.parseInt(args.get(0)));
+                        if (player != null)
+                            return player.getName();
+                        return args.size() == 1 ? "-" : args.get(1);
+                    } catch (NumberFormatException e) {
+                        return "INVALID_ARGS";
+                    }
+                })
                 // Misc
                 .constant("heart", "♥")
                 .constant("newline", "\n")
@@ -79,12 +83,12 @@ public final class Placeholders {
                         return "INVALID_ARGS";
                     }
                 })
-                .variable("year", () -> "TODO")
-                .variable("month", () -> "TODO")
-                .variable("day", () -> "TODO")
-                .variable("hour", () -> "TODO")
-                .variable("minute", () -> "TODO")
-                .variable("second", () -> "TODO")
+                .variable("year", () -> String.valueOf(LocalDate.now().getYear()))
+                .variable("month", () -> toPaddedString(LocalDate.now().getMonthValue()))
+                .variable("day", () -> toPaddedString(LocalDate.now().getDayOfMonth()))
+                .variable("hour", () -> toPaddedString(LocalDateTime.now().getHour()))
+                .variable("minute", () -> toPaddedString(LocalDateTime.now().getMinute()))
+                .variable("second", () -> toPaddedString(LocalDateTime.now().getSecond()))
                 .namespace(null, PlayerContext.class, Function.identity(), Placeholders::addPlayerPlaceholders)
                 .namespace("killer-", KillContext.class, ctx -> new PlayerContext(ctx.getKiller()), Placeholders::addPlayerPlaceholders)
                 .namespace("owner-", ContainerContext.class, ctx -> new PlayerContext(ctx.getOwner()), Placeholders::addPlayerPlaceholders)
@@ -114,18 +118,18 @@ public final class Placeholders {
         .contextual("rank", PlayerContext.class, (ctx) -> ctx.getPlayer().getRank() != null ? ctx.getPlayer().getRank().getDisplay() : "-")
         .contextual("kills", PlayerContext.class, (ctx) -> String.valueOf(ctx.getPlayer().getStats().getKills()))
         .contextual("strikes", PlayerContext.class, (ctx) -> String.valueOf(ctx.getPlayer().getStats().getStrikes().size()))
-        .contextual("countdown-hour", PlayerContext.class, (ctx) -> !Main.getVaroGame().isPlayTimeLimited() ? "-" : String.format("%02d", ctx.getPlayer().getStats().getCountdown() / 3600))
-        .contextual("countdown-minute", PlayerContext.class, (ctx) -> !Main.getVaroGame().isPlayTimeLimited() ? "-" : String.format("%02d", (ctx.getPlayer().getStats().getCountdown() / 60) % 60))
-        .contextual("countdown-second", PlayerContext.class, (ctx) -> !Main.getVaroGame().isPlayTimeLimited() ? "-" : String.format("%02d", ctx.getPlayer().getStats().getCountdown() % 60))
-        .contextual("session-hour", PlayerContext.class, (ctx) -> String.format("%02d", ctx.getPlayer().getStats().getSessionTime() / 3600))
-        .contextual("session-minute", PlayerContext.class, (ctx) -> String.format("%02d", (ctx.getPlayer().getStats().getSessionTime() / 60) % 60))
-        .contextual("session-second", PlayerContext.class, (ctx) -> String.format("%02d", ctx.getPlayer().getStats().getSessionTime() % 60))
-        .contextual("online-hour", PlayerContext.class, (ctx) -> String.format("%02d", ctx.getPlayer().getStats().getOnlineTime() / 3600))
-        .contextual("online-minute", PlayerContext.class, (ctx) -> String.format("%02d", (ctx.getPlayer().getStats().getOnlineTime() / 60) % 60))
-        .contextual("online-second", PlayerContext.class, (ctx) -> String.format("%02d", ctx.getPlayer().getStats().getOnlineTime() % 60))
-        .contextual("online-total-hour", PlayerContext.class, (ctx) -> String.format("%02d", ctx.getPlayer().getStats().getOnlineTimeTotal() / 3600))
-        .contextual("online-total-minute", PlayerContext.class, (ctx) -> String.format("%02d", (ctx.getPlayer().getStats().getOnlineTimeTotal() / 60) % 60))
-        .contextual("online-total-second", PlayerContext.class, (ctx) -> String.format("%02d", ctx.getPlayer().getStats().getOnlineTimeTotal() % 60))
+        .contextual("countdown-hour", PlayerContext.class, (ctx) -> !Main.getVaroGame().isPlayTimeLimited() ? "-" : toPaddedString(ctx.getPlayer().getStats().getCountdown() / 3600))
+        .contextual("countdown-minute", PlayerContext.class, (ctx) -> !Main.getVaroGame().isPlayTimeLimited() ? "-" : toPaddedString((ctx.getPlayer().getStats().getCountdown() / 60) % 60))
+        .contextual("countdown-second", PlayerContext.class, (ctx) -> !Main.getVaroGame().isPlayTimeLimited() ? "-" : toPaddedString(ctx.getPlayer().getStats().getCountdown() % 60))
+        .contextual("session-hour", PlayerContext.class, (ctx) -> toPaddedString(ctx.getPlayer().getStats().getSessionTime() / 3600))
+        .contextual("session-minute", PlayerContext.class, (ctx) -> toPaddedString((ctx.getPlayer().getStats().getSessionTime() / 60) % 60))
+        .contextual("session-second", PlayerContext.class, (ctx) -> toPaddedString(ctx.getPlayer().getStats().getSessionTime() % 60))
+        .contextual("online-hour", PlayerContext.class, (ctx) -> toPaddedString(ctx.getPlayer().getStats().getOnlineTime() / 3600))
+        .contextual("online-minute", PlayerContext.class, (ctx) -> toPaddedString((ctx.getPlayer().getStats().getOnlineTime() / 60) % 60))
+        .contextual("online-second", PlayerContext.class, (ctx) -> toPaddedString(ctx.getPlayer().getStats().getOnlineTime() % 60))
+        .contextual("online-total-hour", PlayerContext.class, (ctx) -> toPaddedString(ctx.getPlayer().getStats().getOnlineTimeTotal() / 3600))
+        .contextual("online-total-minute", PlayerContext.class, (ctx) -> toPaddedString((ctx.getPlayer().getStats().getOnlineTimeTotal() / 60) % 60))
+        .contextual("online-total-second", PlayerContext.class, (ctx) -> toPaddedString(ctx.getPlayer().getStats().getOnlineTimeTotal() % 60))
         .contextual("episode", PlayerContext.class, ctx -> String.valueOf(ctx.getPlayer().getStats().getSessionsPlayed()))
         .contextual("episodes-remaining", PlayerContext.class, ctx -> String.valueOf(ctx.getPlayer().getStats().getSessions()))
         .contextual("remaining-disconnects", PlayerContext.class, ctx -> String.valueOf(VaroPlayerDisconnect.getDisconnect(ctx.getPlayer().getPlayer()) != null ? ConfigSetting.DISCONNECT_PER_SESSION.getValueAsInt() - VaroPlayerDisconnect.getDisconnect(ctx.getPlayer().getPlayer()).getDisconnects() : ConfigSetting.DISCONNECT_PER_SESSION.getValueAsInt()))
@@ -147,5 +151,9 @@ public final class Placeholders {
             .contextual("health", OnlinePlayerContext.class, ctx -> String.valueOf(ctx.getPlayer().getHealth()))
             .contextual("food", OnlinePlayerContext.class, ctx -> String.valueOf(ctx.getPlayer().getFoodLevel()));
         });
+    }
+    
+    private static String toPaddedString(long value) {
+        return value <= 9 ? "0" + value : String.valueOf(value);
     }
 }
