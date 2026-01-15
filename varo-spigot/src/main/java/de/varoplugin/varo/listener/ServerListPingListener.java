55f40c2dcf5a89ef93be05b456417c9ca4d375a0
AlmightySatan <43804777+Almighty-Satan@users.noreply.github.com>
2026-01-15 17:53:38 +0100

    Fix server displaying incorrect motd before the game starts

--- varo-spigot/src/main/java/de/varoplugin/varo/listener/ServerListPingListener.java
+++ varo-spigot/src/main/java/de/varoplugin/varo/listener/ServerListPingListener.java
@@ -1,38 +1,37 @@
 package de.varoplugin.varo.listener;
 
+import de.varoplugin.varo.Main;
+import de.varoplugin.varo.config.language.Messages;
+import de.varoplugin.varo.configuration.configurations.config.ConfigSetting;
 import org.bukkit.Bukkit;
 import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.server.ServerListPingEvent;
 
-import de.varoplugin.varo.Main;
-import de.varoplugin.varo.config.language.Messages;
-import de.varoplugin.varo.configuration.configurations.config.ConfigSetting;
-
 public class ServerListPingListener implements Listener {
 
-	@EventHandler
-	public void onServerListPing(ServerListPingEvent event) {
-		int slots = ConfigSetting.FAKE_MAX_SLOTS.getValueAsInt();
-		if (slots != -1)
-			event.setMaxPlayers(slots);
+    @EventHandler
+    public void onServerListPing(ServerListPingEvent event) {
+        int slots = ConfigSetting.FAKE_MAX_SLOTS.getValueAsInt();
+        if (slots != -1)
+            event.setMaxPlayers(slots);
 
-		if (ConfigSetting.CHANGE_MOTD.getValueAsBoolean()) {
-			if (!Main.getVaroGame().hasStarted()) {
-				if (Bukkit.getServer().hasWhitelist())
-					event.setMotd(Messages.MOTD_OPEN.value());
-				else
-					event.setMotd(Messages.MOTD_CLOSED.value());
-				return;
-			}
+        if (!ConfigSetting.CHANGE_MOTD.getValueAsBoolean())
+            return;
 
-			if (!ConfigSetting.ONLY_JOIN_BETWEEN_HOURS.getValueAsBoolean() || Main.getDataManager().getOutsideTimeChecker().canJoin() || !Main.getVaroGame().hasStarted()) {
-				event.setMotd(Messages.MOTD_OPEN.value());
-				return;
-			}
+        if (!Main.getVaroGame().hasStarted()) {
+            if (Bukkit.getServer().hasWhitelist())
+                event.setMotd(Messages.MOTD_CLOSED.value());
+            else
+                event.setMotd(Messages.MOTD_OPEN.value());
+            return;
+        }
 
-			if (!Main.getDataManager().getOutsideTimeChecker().canJoin())
-			    event.setMotd(Messages.MOTD_CLOSED_HOURS.value());
-		}
-	}
+        if (ConfigSetting.ONLY_JOIN_BETWEEN_HOURS.getValueAsBoolean() && !Main.getDataManager().getOutsideTimeChecker().canJoin()) {
+            event.setMotd(Messages.MOTD_CLOSED_HOURS.value());
+            return;
+        }
+
+        event.setMotd(Messages.MOTD_OPEN.value());
+    }
 }
